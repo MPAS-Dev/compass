2b0d1131dfda09c3fa93da0ace0e52f8a380c92a
Xylar Asay-Davis <xylarstorm@gmail.com>
2020-09-07 16:34:19 +0200

    Update base mesh to use new COMPASS approach
    COMPASS now runs a local script, `build_base_mesh.py` that
    defines the resolution but also calls `build_spherical_mesh`.

--- ocean/cosine_bell/QU60/default/build_base_mesh.py
+++ ocean/cosine_bell/QU60/default/build_base_mesh.py
@@ -7,15 +7,25 @@
 %    lon - longitude, vector of length n, with entries between -180 and 180, degrees
 """
 import numpy as np
+from mpas_tools.ocean import build_spherical_mesh
 
 
 def cellWidthVsLatLon():
 
     ddeg = 10
-    constantCellWidth = 120
+    constantCellWidth = 60
 
     lat = np.arange(-90, 90.01, ddeg)
     lon = np.arange(-180, 180.01, ddeg)
 
     cellWidth = constantCellWidth * np.ones((lat.size, lon.size))
     return cellWidth, lon, lat
+
+
+def main():
+    cellWidth, lon, lat = cellWidthVsLatLon()
+    build_spherical_mesh(cellWidth, lon, lat, out_filename='base_mesh.nc')
+
+
+if __name__ == '__main__':
+    main()
