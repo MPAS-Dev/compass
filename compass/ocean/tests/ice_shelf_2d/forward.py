f2bc102cb882b4e40539aa6e899e077220740ef7
Xylar Asay-Davis <xylarstorm@gmail.com>
2020-12-16 11:26:45 +0100

    Add the default ice_shelf_2d testcase

---
+++ compass/ocean/tests/ice_shelf_2d/forward.py
@@ -0,0 +1,179 @@
+import os
+import numpy
+
+from compass.testcase import get_step_default
+from compass.io import symlink
+from compass import namelist, streams
+from compass.model import partition, run_model
+from compass.parallel import update_namelist_pio
+
+
+def collect(resolution, cores, min_cores=None, max_memory=1000,
+            max_disk=1000, threads=1, testcase_module=None,
+            namelist_file=None, streams_file=None):
+    """
+    Get a dictionary of step properties
+
+    Parameters
+    ----------
+    resolution : {'5km'}
+        The name of the resolution to run at
+
+    cores : int
+        The number of cores to run on in forward runs. If this many cores are
+        available on the machine or batch job, the task will run on that number.
+        If fewer are available (but no fewer than min_cores), the job will
+        run on all available cores instead.
+
+    min_cores : int, optional
+        The minimum allowed cores.  If that number of cores are not available on
+        the machine or in the batch job, the run will fail.  By default,
+        ``min_cores = cores``
+
+    max_memory : int, optional
+        The maximum amount of memory (in MB) this step is allowed to use
+
+    max_disk : int, optional
+        The maximum amount of disk space  (in MB) this step is allowed to use
+
+    threads : int, optional
+        The number of threads to run with during forward runs
+
+    testcase_module : str, optional
+        The module for the testcase
+
+    namelist_file : str, optional
+        The name of a namelist file in the testcase package directory
+
+    streams_file : str, optional
+        The name of a streams file in the testcase package directory
+
+    Returns
+    -------
+    step : dict
+        A dictionary of properties of this step
+    """
+    step = get_step_default(__name__)
+    step['resolution'] = resolution
+    step['cores'] = cores
+    step['max_memory'] = max_memory
+    step['max_disk'] = max_disk
+    if min_cores is None:
+        min_cores = cores
+    step['min_cores'] = min_cores
+    step['threads'] = threads
+    if testcase_module is not None:
+        step['testcase_module'] = testcase_module
+    else:
+        if namelist_file is not None or streams_file is not None:
+            raise ValueError('You must supply a testcase module for the '
+                             'namelist and/or streams file')
+    if namelist_file is not None:
+        step['namelist'] = namelist_file
+    if streams_file is not None:
+        step['streams'] = streams_file
+
+    return step
+
+
+def setup(step, config):
+    """
+    Set up the test case in the work directory, including downloading any
+    dependencies
+
+    Parameters
+    ----------
+    step : dict
+        A dictionary of properties of this step from the ``collect()`` function
+
+    config : configparser.ConfigParser
+        Configuration options for this testcase, a combination of the defaults
+        for the machine, core, configuration and testcase
+    """
+    step_dir = step['work_dir']
+
+    # generate the namelist, replacing a few default options
+    replacements = dict()
+
+    # defaults when global stats are enabled
+    replacements.update(namelist.parse_replacements(
+        'compass.ocean.namelists.analysis', 'namelist.global_stats'))
+
+    replacements.update(namelist.parse_replacements(
+        'compass.ocean.tests.ice_shelf_2d', 'namelist.forward'))
+
+    if 'testcase_module' in step:
+        testcase_module = step['testcase_module']
+    else:
+        testcase_module = None
+
+    # see if there's one for the testcase itself
+    if 'namelist' in step:
+        replacements.update(namelist.parse_replacements(
+            testcase_module, step['namelist']))
+
+    namelist.generate(config=config, replacements=replacements,
+                      step_work_dir=step_dir, core='ocean', mode='forward')
+
+    # generate the streams file
+    streams_data = streams.read('compass.ocean.streams',
+                                'streams.land_ice_fluxes')
+
+    streams_data = streams.read('compass.ocean.tests.ice_shelf_2d',
+                                'streams.forward', tree=streams_data)
+
+    # see if there's one for the testcase itself
+    if 'streams' in step:
+        streams_data = streams.read(testcase_module, step['streams'],
+                                    tree=streams_data)
+
+    streams.generate(config=config, tree=streams_data, step_work_dir=step_dir,
+                     core='ocean', mode='forward')
+
+    # make a link to the ocean_model executable
+    symlink(os.path.abspath(config.get('executables', 'model')),
+            os.path.join(step_dir, 'ocean_model'))
+
+    inputs = []
+    outputs = []
+
+    links = {'../ssh_adjustment/adjusted_init.nc': 'init.nc',
+             '../initial_state/culled_graph.info': 'graph.info'}
+    for target, link in links.items():
+        symlink(target, os.path.join(step_dir, link))
+        inputs.append(os.path.abspath(os.path.join(step_dir, target)))
+
+    for file in ['output.nc']:
+        outputs.append(os.path.join(step_dir, file))
+
+    step['inputs'] = inputs
+    step['outputs'] = outputs
+
+
+def run(step, test_suite, config, logger):
+    """
+    Run this step of the testcase
+
+    Parameters
+    ----------
+    step : dict
+        A dictionary of properties of this step from the ``collect()`` function,
+        with modifications from the ``setup()`` function.
+
+    test_suite : dict
+        A dictionary of properties of the test suite
+
+    config : configparser.ConfigParser
+        Configuration options for this testcase, a combination of the defaults
+        for the machine, core and configuration
+
+    logger : logging.Logger
+        A logger for output from the step
+    """
+    cores = step['cores']
+    threads = step['threads']
+    step_dir = step['work_dir']
+    update_namelist_pio(config, cores, step_dir)
+    partition(cores, logger)
+    run_model(config, core='ocean', core_count=cores, logger=logger,
+              threads=threads)
---
+++ compass/ocean/tests/ice_shelf_2d/initial_state.py
@@ -0,0 +1,184 @@
+import os
+import xarray
+import numpy
+
+from mpas_tools.planar_hex import make_planar_hex_mesh
+from mpas_tools.io import write_netcdf
+from mpas_tools.mesh.conversion import convert, cull
+from mpas_tools.cime.constants import constants
+
+from compass.testcase import get_step_default
+from compass.ocean.vertical import generate_grid
+from compass.ocean.iceshelf import compute_land_ice_pressure_and_draft
+from compass.ocean.vertical.zstar import compute_layer_thickness_and_zmid
+
+
+def collect(resolution):
+    """
+    Get a dictionary of step properties
+
+    Parameters
+    ----------
+    resolution : {'5km'}
+        The name of the resolution to run at
+
+    Returns
+    -------
+    step : dict
+        A dictionary of properties of this step
+    """
+    step = get_step_default(__name__)
+    step['resolution'] = resolution
+
+    step['cores'] = 1
+    step['min_cores'] = 1
+    # Maximum allowed memory and disk usage in MB
+    step['max_memory'] = 8000
+    step['max_disk'] = 8000
+
+    return step
+
+
+def setup(step, config):
+    """
+    Set up the test case in the work directory, including downloading any
+    dependencies
+
+    Parameters
+    ----------
+    step : dict
+        A dictionary of properties of this step from the ``collect()`` function
+
+    config : configparser.ConfigParser
+        Configuration options for this testcase, a combination of the defaults
+        for the machine, core, configuration and testcase
+    """
+    step_dir = step['work_dir']
+
+    inputs = []
+    outputs = []
+
+    for file in ['base_mesh.nc', 'culled_mesh.nc', 'culled_graph.info',
+                 'initial_state.nc']:
+        outputs.append(os.path.join(step_dir, file))
+
+    step['inputs'] = inputs
+    step['outputs'] = outputs
+
+
+def run(step, test_suite, config, logger):
+    """
+    Run this step of the testcase
+
+    Parameters
+    ----------
+    step : dict
+        A dictionary of properties of this step from the ``collect()`` function,
+        with modifications from the ``setup()`` function.
+
+    test_suite : dict
+        A dictionary of properties of the test suite
+
+    config : configparser.ConfigParser
+        Configuration options for this testcase, a combination of the defaults
+        for the machine, core and configuration
+
+    logger : logging.Logger
+        A logger for output from the step
+   """
+    section = config['ice_shelf_2d']
+    nx = section.getint('nx')
+    ny = section.getint('ny')
+    dc = section.getfloat('dc')
+
+    dsMesh = make_planar_hex_mesh(nx=nx, ny=ny, dc=dc, nonperiodic_x=False,
+                                  nonperiodic_y=True)
+    write_netcdf(dsMesh, 'base_mesh.nc')
+
+    dsMesh = cull(dsMesh, logger=logger)
+    dsMesh = convert(dsMesh, graphInfoFileName='culled_graph.info',
+                     logger=logger)
+    write_netcdf(dsMesh, 'culled_mesh.nc')
+
+    section = config['ice_shelf_2d']
+    vert_levels = section.getint('vert_levels')
+    bottom_depth = section.getfloat('bottom_depth')
+    temperature = section.getfloat('temperature')
+    surface_salinity = section.getfloat('surface_salinity')
+    bottom_salinity = section.getfloat('bottom_salinity')
+
+    # points 1 and 2 are where angles on ice shelf are located.
+    # point 3 is at the surface.
+    # d variables are total water-column thickness below ice shelf
+    y1 = section.getfloat('y1')
+    y2 = section.getfloat('y2')
+    y3 = y2 + section.getfloat('edge_width')
+    d1 = section.getfloat('cavity_thickness')
+    d2 = d1 + section.getfloat('slope_height')
+    d3 = bottom_depth
+
+    ds = dsMesh.copy()
+
+    interfaces = generate_grid(grid_type='uniform', vert_levels=vert_levels,
+                               max_depth=bottom_depth)
+
+    ds['refBottomDepth'] = ('nVertLevels', interfaces[1:])
+    ds['refZMid'] = ('nVertLevels', -0.5 * (interfaces[1:] + interfaces[0:-1]))
+    ds['vertCoordMovementWeights'] = xarray.ones_like(ds.refBottomDepth)
+
+    yCell = ds.yCell
+    ds['bottomDepth'] = bottom_depth * xarray.ones_like(yCell)
+    ds['maxLevelCell'] = vert_levels * xarray.ones_like(yCell, dtype=int)
+
+    column_thickness = xarray.where(yCell < y1, d1,
+                                    d1 + (d2 - d1) * (yCell - y1) / (y2 - y1))
+    column_thickness = xarray.where(yCell < y2, column_thickness,
+                                    d2 + (d3 - d2) * (yCell - y2) / (y3 - y2))
+    column_thickness = xarray.where(yCell < y3, column_thickness, d3)
+
+    ssh = -bottom_depth + column_thickness
+
+    cellMask = xarray.ones_like(yCell)
+    cellMask, _ = xarray.broadcast(cellMask, ds.refBottomDepth)
+    cellMask = cellMask.transpose('nCells', 'nVertLevels')
+
+    restingThickness, layerThickness, zMid = compute_layer_thickness_and_zmid(
+        cellMask, ds.refBottomDepth, ds.bottomDepth, ds.maxLevelCell, ssh=ssh)
+
+    layerThickness = layerThickness.expand_dims(dim='Time', axis=0)
+    ssh = ssh.expand_dims(dim='Time', axis=0)
+    modifySSHMask = xarray.where(yCell < y3, 1, 0).expand_dims(
+        dim='Time', axis=0)
+    landIceFraction = modifySSHMask.astype(float)
+    landIceMask = modifySSHMask.copy()
+
+    ref_density = constants['SHR_CONST_RHOSW']
+    landIcePressure, landIceDraft = compute_land_ice_pressure_and_draft(
+        ssh=ssh, modifySSHMask=modifySSHMask, ref_density=ref_density)
+
+    salinity = surface_salinity + ((bottom_salinity - surface_salinity) *
+                                   (zMid / (-bottom_depth)))
+    salinity, _ = xarray.broadcast(salinity, layerThickness)
+    salinity = salinity.transpose('Time', 'nCells', 'nVertLevels')
+
+    normalVelocity = xarray.zeros_like(ds.xEdge)
+    normalVelocity, _ = xarray.broadcast(normalVelocity, ds.refBottomDepth)
+    normalVelocity = normalVelocity.transpose('nEdges', 'nVertLevels')
+    normalVelocity = normalVelocity.expand_dims(dim='Time', axis=0)
+
+    ds['temperature'] = temperature * xarray.ones_like(layerThickness)
+    ds['salinity'] = salinity
+    ds['normalVelocity'] = normalVelocity
+    ds['layerThickness'] = layerThickness
+    ds['ssh'] = ssh
+    ds['restingThickness'] = restingThickness
+    ds['fCell'] = xarray.zeros_like(ds.xCell)
+    ds['fEdge'] = xarray.zeros_like(ds.xEdge)
+    ds['fVertex'] = xarray.zeros_like(ds.xVertex)
+    ds['modifySSHMask'] = modifySSHMask
+    ds['landIceFraction'] = landIceFraction
+    ds['landIceMask'] = landIceMask
+    ds['landIcePressure'] = landIcePressure
+    ds['landIceDraft'] = landIceDraft
+
+    write_netcdf(ds, 'initial_state.nc')
