0e6f427bdb47920d7aa06b9abace3ca621a9c3df
Xylar Asay-Davis <xylarstorm@gmail.com>
2021-01-27 16:31:02 +0100

    Update setup.py to include package_data files
    Also, improve how the compass package is found (with "include",
    not "exclude").

--- setup.py
+++ setup.py
@@ -4,6 +4,20 @@ import os
 import re
 from setuptools import setup, find_packages
 
+
+def package_files(directory, prefixes, extensions):
+    """ based on https://stackoverflow.com/a/36693250/7728169"""
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            parts = filename.split('.')
+            prefix = parts[0]
+            extension = parts[-1]
+            if prefix in prefixes or extension in extensions:
+                paths.append(os.path.join('..', path, filename))
+    return paths
+
+
 here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'compass', '__init__.py')) as f:
     init_file = f.read()
@@ -12,6 +26,10 @@ version = re.search(r'{}\s*=\s*[(]([^)]*)[)]'.format('__version_info__'),
                     init_file).group(1).replace(', ', '.')
 
 os.chdir(here)
+
+data_files = package_files('compass', prefixes=['namelist', 'streams'],
+                           extensions=['cfg', 'template', 'json', 'txt',
+                                       'geojson'])
 
 setup(name='compass',
       version=version,
@@ -39,9 +57,8 @@ setup(name='compass',
           'Programming Language :: Python :: 3.9',
           'Topic :: Scientific/Engineering',
       ],
-      packages=find_packages(exclude=['ci', 'deploy', 'docs', 'landice',
-                                      'MPAS-Mode', 'ocean', 'recipe', 'test',
-                                      'utility_scripts']),
+      packages=find_packages(include=['compass', 'compass.*']),
+      package_data={'': data_files},
       install_requires=['matplotlib',
                         'netCDF4',
                         'numpy',
