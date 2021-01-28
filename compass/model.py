73d1c8402603b183b0d1cb4732c491acac771d7e
Xylar Asay-Davis <xylarstorm@gmail.com>
2021-01-28 17:07:52 +0100

    Rewrite run_model to take the step dict and do more
    Since we almost always want to:
    * update PIO
    * make a graph partiton
    * run the model
    these first two are now an optional part of running the model.
    By having run_model take `step`, we can get information like the
    number of cores, threads and work dir. from there.
    
    To make this easier, the core and configration were added to `step`
    while setting up `testcase` defaults.  There could be a little
    confusion between "core" and "cores" (and "configuration" and
    "config file" for that matter) but this is hard to avoid.

--- compass/model.py
+++ compass/model.py
@@ -1,86 +1,151 @@
 import os
+import numpy
 
 from mpas_tools.logging import check_call
 
-from compass.io import symlink
+from compass.namelist import update
+from compass.io import add_input_file
 
 
-def symlink_model(config, step_dir):
+def add_model_as_input(step, config):
     """
-    make a link to the model executable
+    make a link to the model executable and add it to the inputs
 
     Parameters
     ----------
-    config : configparser.ConfigParser
-        Configuration options for the testcase
+    step : dict
+        A dictionary of properties of this step
 
-    step_dir : str
-        The work directory for the step where the model link should be added
+    config : configparser.ConfigParser
+        Configuration options for the test case
     """
     model = config.get('executables', 'model')
     model_basename = os.path.basename(model)
-    symlink(os.path.abspath(model), os.path.join(step_dir, model_basename))
+    add_input_file(step, filename=model_basename,
+                   target=os.path.abspath(model))
 
 
-def partition(core_count, logger, executable='gpmetis',
-              graph_file='graph.info'):
-    """
-    Partition the domain for the requested number of cores
-
-    Parameters
-    ----------
-    core_count : int
-        The number of cores that the model should be run on
-
-    logger : logging.Logger
-        A logger for output from the step that is calling this function
-
-    executable : str, optional
-        The command to use for partitioning
-
-    graph_file : str, optional
-        The name of the graph file to partition
-
-    """
-    args = [executable, graph_file, '{}'.format(core_count)]
-    check_call(args, logger)
-
-
-def run_model(config, core, core_count, logger, threads=1):
+def run_model(step, config, logger, update_pio=True, partition_graph=True,
+              graph_file='graph.info', namelist=None, streams=None):
     """
     Run the model after determining the number of cores
 
     Parameters
     ----------
+    step : dict
+        A dictionary of properties of this step
+
     config : configparser.ConfigParser
-        Configuration options for the testcase
-
-    core : str
-        The name of the MPAS core ('ocean', 'landice', etc.)
-
-    core_count : int
-        The maximum number of cores that the model should be run on
+        Configuration options for the test case
 
     logger : logging.Logger
         A logger for output from the step that is calling this function
 
-    threads : int
-        The number of threads to use for the model run
+    update_pio : bool, optional
+        Whether to modify the namelist so the number of PIO tasks and the
+        stride between them is consistent with the number of nodes and cores
+        (one PIO task per node).
+
+    partition_graph : bool, optional
+        Whether to partition the domain for the requested number of cores.  If
+        so, the partitioning executable is taken from the ``partition`` option
+        of the ``[executables]`` config section.
+
+    graph_file : str, optional
+        The name of the graph file to partition
+
+    namelist : str, optional
+        The name of the namelist file, default is ``namelist.<core>``
+
+    streams : str, optional
+        The name of the streams file, default is ``streams.<core>``
     """
+    core = step['core']
+    cores = step['cores']
+    threads = step['threads']
+    step_dir = step['work_dir']
+
+    if update_pio:
+        update_namelist_pio(config, cores, step_dir)
+
+    if partition_graph:
+        partition(cores, config, logger, graph_file=graph_file)
+
     os.environ['OMP_NUM_THREADS'] = '{}'.format(threads)
 
-    namelist = 'namelist.{}'.format(core)
+    if namelist is None:
+        namelist = 'namelist.{}'.format(core)
 
-    streams = 'streams.{}'.format(core)
+    if streams is None:
+        streams = 'streams.{}'.format(core)
 
     parallel_executable = config.get('parallel', 'parallel_executable')
     model = config.get('executables', 'model')
     model_basename = os.path.basename(model)
 
     args = [parallel_executable,
-            '-n', '{}'.format(core_count),
+            '-n', '{}'.format(cores),
             './{}'.format(model_basename),
             '-n', namelist,
             '-s', streams]
 
     check_call(args, logger)
+
+
+def partition(cores, config, logger, graph_file='graph.info'):
+    """
+    Partition the domain for the requested number of cores
+
+    Parameters
+    ----------
+    cores : int
+        The number of cores that the model should be run on
+
+    config : configparser.ConfigParser
+        Configuration options for the test case, used to get the partitioning
+        executable
+
+    logger : logging.Logger
+        A logger for output from the step that is calling this function
+
+    graph_file : str, optional
+        The name of the graph file to partition
+
+    """
+    executable = config.get('parallel', 'partition_executable')
+    args = [executable, graph_file, '{}'.format(cores)]
+    check_call(args, logger)
+
+
+def update_namelist_pio(config, cores, step_dir):
+    """
+    Modify the namelist so the number of PIO tasks and the stride between them
+    is consistent with the number of nodes and cores (one PIO task per node).
+
+    Parameters
+    ----------
+     config : configparser.ConfigParser
+        Configuration options for this test case
+
+    cores : int
+        The number of cores
+
+    step_dir : str
+        The work directory for this step of the test case
+    """
+
+    cores_per_node = config.getint('parallel', 'cores_per_node')
+
+    # update PIO tasks based on the machine settings and the available number
+    # or cores
+    pio_num_iotasks = int(numpy.ceil(cores/cores_per_node))
+    pio_stride = cores//pio_num_iotasks
+    if pio_stride > cores_per_node:
+        raise ValueError('Not enough nodes for the number of cores.  cores: '
+                         '{}, cores per node: {}'.format(cores,
+                                                         cores_per_node))
+
+    replacements = {'config_pio_num_iotasks': '{}'.format(pio_num_iotasks),
+                    'config_pio_stride': '{}'.format(pio_stride)}
+
+    update(replacements=replacements, step_work_dir=step_dir, core='ocean')
--- compass/parallel.py
+++ compass/parallel.py
@@ -1,9 +1,6 @@
 import os
 import multiprocessing
 import subprocess
-import numpy
-
-from compass import namelist, streams
 
 
 def get_available_cores_and_nodes(config):
@@ -13,7 +10,7 @@ def get_available_cores_and_nodes(config):
     Parameters
     ----------
     config : configparser.ConfigParser
-        Configuration options for the testcase
+        Configuration options for the test case
 
     Returns
     -------
@@ -39,41 +36,6 @@ def get_available_cores_and_nodes(config):
             parallel_system))
 
     return cores, nodes
-
-
-def update_namelist_pio(config, cores, step_dir):
-    """
-    Modify the namelist so the number of PIO tasks and the stride between them
-    is consistent with the number of nodes and cores (one PIO task per node).
-
-    Parameters
-    ----------
-     config : configparser.ConfigParser
-        Configuration options for this testcase
-
-    cores : int
-        The number of cores
-
-    step_dir : str
-        The work directory for this step of the testcase
-    """
-
-    cores_per_node = config.getint('parallel', 'cores_per_node')
-
-    # update PIO tasks based on the machine settings and the available number
-    # or cores
-    pio_num_iotasks = int(numpy.ceil(cores/cores_per_node))
-    pio_stride = cores//pio_num_iotasks
-    if pio_stride > cores_per_node:
-        raise ValueError('Not enough nodes for the number of cores.  cores: '
-                         '{}, cores per node: {}'.format(cores,
-                                                         cores_per_node))
-
-    replacements = {'config_pio_num_iotasks': '{}'.format(pio_num_iotasks),
-                    'config_pio_stride': '{}'.format(pio_stride)}
-
-    namelist.update(replacements=replacements, step_work_dir=step_dir,
-                    core='ocean')
 
 
 def _get_subprocess_int(args):
