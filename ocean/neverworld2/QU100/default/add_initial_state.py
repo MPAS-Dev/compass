3ab5bb28cb40309cc30fc2abbde545569449b9c2
Alice Barthel <abarthel@lanl.gov>
2021-02-14 10:31:09 -0700

    added midAtlantic ridge and Drake Passage sill

--- ocean/neverworld2/QU100/default/add_initial_state.py
+++ ocean/neverworld2/QU100/default/add_initial_state.py
@@ -120,6 +120,8 @@ def main():
     T.add_NS_coast(NW2_lonE, -90, -60, cw, cd)
     T.add_EW_coast(-360, 360, NW2_latS, cw, cd)
     T.add_EW_coast(-360, 360, NW2_latN, cw, cd)
+    T.add_circular_ridge(NW2_lonW, -50, 10, 2, drake) # Drake sill
+    T.add_NS_ridge(30, -90, 90, 30, D0/2) # Mid-Atlantic ridge
     bottomDepthObserved[:] = -T.z[:]
     print('Depth range: ', min(bottomDepthObserved), max(bottomDepthObserved))
     # Compute maxLevelCell and layerThickness for z-level (variation only on top)
