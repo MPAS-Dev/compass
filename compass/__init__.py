1d96c75c400d186bfdf5ca7909da55b755e7d4ac
Xylar Asay-Davis <xylarstorm@gmail.com>
2021-04-08 22:46:47 +0200

    Import the new classes directly in compass
    This makes them easier to refer to in docstrings

--- compass/__init__.py
+++ compass/__init__.py
@@ -1,2 +1,7 @@
+from compass.mpas_core import MpasCore
+from compass.testgroup import TestGroup
+from compass.testcase import TestCase
+from compass.step import Step
+
 __version_info__ = (1, 0, 0)
 __version__ = '.'.join(str(vi) for vi in __version_info__)
