a9c34719520855f7e474be4f04a1c733328245e9
Xylar Asay-Davis <xylarstorm@gmail.com>
2021-04-08 22:44:05 +0200

    Add a class TestCase
    Make most functions from the testcase module into methods of
    ``TestCase``.

--- compass/testcase/__init__.py
+++ compass/testcase/__init__.py
@@ -1,5 +1,4 @@
 import os
-import sys
 import stat
 from jinja2 import Template
 from importlib import resources
@@ -8,346 +7,222 @@ from mpas_tools.logging import LoggingContext
 from compass.parallel import get_available_cores_and_nodes
 
 
-def add_testcase(testcases, module, **kwargs):
+class TestCase:
     """
-    Add a test case to a list of test cases in a configuration.  This should
-    be called for each test case in a configuration's ``collect()`` function.
+    The base class for test cases---such as a decomposition, threading or
+    restart test---that are made up of one or more steps
 
-    Any additional keyword arguments passed to this function will be added
-    as keys and values to the ``testcase`` dictionary
-
-    Parameters
+    Attributes
     ----------
-    testcases : `list`
-        A list of test-case dictionaries to which the one for this test case
-        should be appended
+    name : str
+        the name of the test case
 
-    module : `module`
-        The python module containing the test case
+    mpas_core
 
-    **kwargs
-        Additional keyword arguments.
+    test_group
 
-    Returns
-    -------
-    testcase : `dict`
-        A dictionary of information about the test case
-    """
+    steps : dict
+        A dictionary of steps in the test case with step names as keys
 
-    testcase = _get_testcase_default(module.__name__)
-    testcase.update(kwargs)
-    module.collect(testcase)
-    if testcase['description'] is None:
-        raise ValueError('No description was added for {}'.format(
-            testcase['path']))
-    testcases.append(testcase)
-
-    return testcase
-
-
-def set_testcase_subdir(testcase, subdir):
-    """
-    Replaces the default subdirectory and relative path in the test case using
-    the given subdirectory.  This should be called in the test case's
-    ``collect()`` function if the default subdirectory (the name of the test
-    case) is not used.
-
-    Parameters
-    ----------
-    testcase : `dict`
-        A dictionary of information about the test case
-
-    subdir : `str`
-        A subdirectory for the test case within the configuration
-    """
-    testcase['subdir'] = subdir
-    testcase['path'] = os.path.join(testcase['core'],
-                                    testcase['configuration'], subdir)
-
-
-def add_step(testcase, module, **kwargs):
-    """
-    Add a step to the dictionary of steps in the test case.  This should
-    be called for each step in a test case's ``collect()`` function.
-
-    Any additional keyword arguments passed to this function will be added
-    as keys and values to the ``step`` dictionary
-
-    Parameters
-    ----------
-    testcase : `dict`
-        A dictionary of information about the test case
-
-    module : `module`
-        The python module containing the step
-
-    **kwargs
-        Additional keyword arguments.
-
-    Returns
-    -------
-    step : `dict`
-        A dictionary of information about the step
-    """
-
-    step = _get_step_default(module.__name__, testcase)
-    step.update(kwargs)
-    module.collect(testcase, step)
-    testcase['steps'][step['name']] = step
-    testcase['steps_to_run'].append(step['name'])
-    return step
-
-
-def run_steps(testcase, test_suite, config, logger):
-    """
-    Run the requested steps of a test case
-
-    Parameters
-    ----------
-    testcase : dict
-        The dictionary describing the test case with info from
-        :py:func:`compass.testcase.get_testcase_default()` and any additional
-        information added when collecting and setting up the test case.
-
-    test_suite : dict
-        A dictionary of properties of the test suite
+    steps_to_run : list
+        A list of the steps to run when ``run()`` gets called.  This list
+        includes all steps by default but can be replaced with a list of only
+        those tests that should run by default if some steps are optional and
+        should be run manually by the user.
 
     config : configparser.ConfigParser
-        Configuration options for this test case
+        Configuration options for this test case, a combination of the
+        defaults for the machine, MPAS core, test group, and any added in
+        this test case's ``configure()`` method
 
     logger : logging.Logger
-        A logger for output from the test case
+        A logger for output from the test case (either to stdout/stderr or to
+        a log file)
+
+    path
+
+    subdir
+
+    new_step_log_file
+
+    log_filename
     """
-    cwd = os.getcwd()
-    for step_name in testcase['steps_to_run']:
-        step = testcase['steps'][step_name]
-        new_log_file = testcase['new_step_log_file']
-        if 'log_filename' in testcase:
-            step['log_filename'] = testcase['log_filename']
-            do_local_logging = True
+
+    def __init__(self, name, subdir=None):
+        """
+        Create a new test case
+
+        Parameters
+        ----------
+        name : str
+            the name of the test case
+
+        subdir : str, optional
+            the subdirectory for the test case.  The default is ``name``
+        """
+        self.name = name
+        self.mpas_core = None
+        self.test_group = None
+
+        self.steps = dict()
+        self.steps_to_run = list()
+
+        self.config = None
+        self.config_filename = None
+
+        self.logger = None
+        self.log_filename = None
+
+        if subdir is not None:
+            self.subdir = subdir
         else:
-            # We only want to do local log output if the step output is being
-            # redirected to a file.  Otherwise, we assume we're probably just
-            # running one step and the local logging is redundant and
-            # unnecessary
-            do_local_logging = new_log_file
+            self.subdir = name
 
-        if do_local_logging:
-            logger.info(' * Running {}'.format(step_name))
-        try:
-            run_step(step, test_suite, config, logger, new_log_file)
-        except BaseException:
+        self.path = None
+        self.new_step_log_file = True
+        self.work_dir = None
+        self.base_work_dir = None
+
+    def add_step(self, step, run_by_default=True):
+        """
+        Add a step to the test case
+
+        Parameters
+        ----------
+        step : compass.Step
+            The step to add
+
+        run_by_default : bool, optional
+            Whether to add this step to the list of steps to run when the
+            ``run()`` method gets called.  If ``run_by_default=False``, users
+            would need to run this step manually.
+        """
+        self.steps[step.name] = step
+        step.test_case = self
+        if run_by_default:
+            self.steps_to_run.append(step.name)
+
+    def configure(self):
+        """
+        Modify the configuration options for this test case. Test cases should
+        override this method if they want to add config options specific to
+        the test case, e.g. from a config file stored in the test case's python
+        package
+        """
+        pass
+
+    def run(self):
+        """
+        Run each step of the test case.  Test cases can override this method
+        to perform additional operations in addition to running the test case's
+        steps
+
+        """
+        logger = self.logger
+        cwd = os.getcwd()
+        for step_name in self.steps_to_run:
+            step = self.steps[step_name]
+            step.config = self.config
+            new_log_file = self.new_step_log_file
+            if self.log_filename is not None:
+                step.log_filename = self.log_filename
+                do_local_logging = True
+            else:
+                # We only want to do local log output if the step output is
+                # being redirected to a file.  Otherwise, we assume we're
+                # probably just running one step and the local logging is
+                # redundant and unnecessary
+                do_local_logging = new_log_file
+
             if do_local_logging:
-                logger.info('     Failed')
-            raise
+                logger.info(' * Running {}'.format(step_name))
+            try:
+                self._run_step(step, new_log_file)
+            except BaseException:
+                if do_local_logging:
+                    logger.info('     Failed')
+                raise
 
-        if do_local_logging:
-            logger.info('     Complete')
+            if do_local_logging:
+                logger.info('     Complete')
 
-        os.chdir(cwd)
+            os.chdir(cwd)
 
+    def generate(self):
+        """
+        Generate a ``run.py`` script for the test case or step.
+        """
 
-def run_step(step, test_suite, config, logger, new_log_file):
-    """
-    Run the requested step of a test case
+        template = Template(
+            resources.read_text('compass.testcase', 'testcase.template'))
+        test_case = {'name': self.name,
+                     'config_filename': self.config_filename}
+        work_dir = self.work_dir
+        script = template.render(test_case=test_case)
 
-    Parameters
-    ----------
-    step : dict
-        The dictionary describing the step with info from
-        :py:func:`compass.testcase.get_step_default()` and any additional
-        information added when collecting and setting up the step.
+        run_filename = os.path.join(work_dir, 'run.py')
+        with open(run_filename, 'w') as handle:
+            handle.write(script)
 
-    test_suite : dict
-        A dictionary of properties of the test suite
+        # make sure it has execute permission
+        st = os.stat(run_filename)
+        os.chmod(run_filename, st.st_mode | stat.S_IEXEC)
 
-    config : configparser.ConfigParser
-        Configuration options for this test case
+    def _run_step(self, step, new_log_file):
+        """
+        Run the requested step
 
-    logger : logging.Logger
-        A logger for output from the test case
+        Parameters
+        ----------
+        step : compass.Step
+            The step to run
 
-    new_log_file : bool
-        Whether to log to a new log file
-    """
-    cwd = os.getcwd()
-    step_name = step['name']
-    if 'cores' in step:
+        new_log_file : bool
+            Whether to log to a new log file
+        """
+        logger = self.logger
+        config = self.config
+        cwd = os.getcwd()
         available_cores, _ = get_available_cores_and_nodes(config)
-        step['cores'] = min(step['cores'], available_cores)
-    else:
-        logger.warning('Core count not specified for step {}. Default is '
-                       '1 core.'.format(step_name))
-        step['cores'] = 1
-    if 'min_cores' in step:
-        if step['cores'] < step['min_cores']:
-            raise ValueError(
-                'Available cores for {} is below the minimum of {}'
-                ''.format(step['cores'], step['min_cores']))
+        step.cores = min(step.cores, available_cores)
+        if step.min_cores is not None:
+            if step.cores < step.min_cores:
+                raise ValueError(
+                    'Available cores for {} is below the minimum of {}'
+                    ''.format(step.cores, step.min_cores))
 
-    missing_files = list()
-    for input_file in step['inputs']:
-        if not os.path.exists(input_file):
-            missing_files.append(input_file)
+        missing_files = list()
+        for input_file in step.inputs:
+            if not os.path.exists(input_file):
+                missing_files.append(input_file)
 
-    if len(missing_files) > 0:
-        raise OSError(
-            'input file(s) missing in step {} of {}/{}/{}: {}'.format(
-                step_name, step['core'], step['configuration'],
-                step['testcase_subdir'], missing_files))
+        if len(missing_files) > 0:
+            raise OSError(
+                'input file(s) missing in step {} of {}/{}/{}: {}'.format(
+                    step.name, step.mpas_core.name, step.test_group.name,
+                    step.test_case.subdir, missing_files))
 
-    test_name = step['path'].replace('/', '_')
-    if new_log_file:
-        log_filename = '{}/{}.log'.format(cwd, step_name)
-        step['log_filename'] = log_filename
-        step_logger = None
-    else:
-        step_logger = logger
-        log_filename = None
-    with LoggingContext(name=test_name, logger=step_logger,
-                        log_filename=log_filename) as step_logger:
-        run = getattr(sys.modules[step['module']], step['run'])
-        os.chdir(step['work_dir'])
+        test_name = step.path.replace('/', '_')
+        if new_log_file:
+            log_filename = '{}/{}.log'.format(cwd, step.name)
+            step.log_filename = log_filename
+            step_logger = None
+        else:
+            step_logger = logger
+            log_filename = None
+        with LoggingContext(name=test_name, logger=step_logger,
+                            log_filename=log_filename) as step_logger:
+            step.logger = step_logger
+            os.chdir(step.work_dir)
+            step.run()
 
-        run(step, test_suite, config, step_logger)
+        missing_files = list()
+        for output_file in step.outputs:
+            if not os.path.exists(output_file):
+                missing_files.append(output_file)
 
-    missing_files = list()
-    for output_file in step['outputs']:
-        if not os.path.exists(output_file):
-            missing_files.append(output_file)
-
-    if len(missing_files) > 0:
-        raise OSError(
-            'output file(s) missing in step {} of {}/{}/{}: {}'.format(
-                step_name, step['core'], step['configuration'],
-                step['testcase_subdir'], missing_files))
-
-
-def generate_run(template_name, testcase, step=None):
-    """
-    Generate a ``run.py`` script for the given test case or step.
-
-    Parameters
-    ----------
-    template_name : str
-        The name of the template file to use to create the run script
-
-    testcase : dict
-        The dictionary of information about the test case, used to fill in the
-        script template
-
-    step : dict, optional
-        The dictionary of information about the step, used to fill in the
-        script template
-    """
-
-    template = Template(resources.read_text('compass.testcase', template_name))
-    kwargs = {'testcase': testcase}
-    if step is None:
-        work_dir = testcase['work_dir']
-    else:
-        work_dir = step['work_dir']
-        kwargs['step'] = step
-    script = template.render(**kwargs)
-
-    run_filename = os.path.join(work_dir, 'run.py')
-    with open(run_filename, 'w') as handle:
-        handle.write(script)
-
-    # make sure it has execute permission
-    st = os.stat(run_filename)
-    os.chmod(run_filename, st.st_mode | stat.S_IEXEC)
-
-
-def _get_step_default(module, testcase):
-    """
-    Set up a default dictionary describing the step in the given module.  The
-    dictionary contains the full name of the python module for the step, the
-    name of the step (the name of the python file without the ``.py``
-    extension), the subdirectory for the step (the same as the ``name``),
-    the names of the ``setup()`` and ``run()`` functions within the module,
-    and empty lists of ``inputs`` and ``outputs``, to be filled with the
-    files required to run the step or produced by the step, respectively.
-
-    Parameters
-    ----------
-    module : str
-        The full name of the python module for the step, usually supplied from
-        ``__name__``
-
-    testcase : dict
-        A dictionary with information about the test case that this step
-        belongs to
-
-    Returns
-    -------
-    step : dict
-        A dictionary with the default information about the step, most of which
-        can be modified as appropriate
-
-    """
-    name = module.split('.')[-1]
-    # some steps may not need a setup function
-    if hasattr(sys.modules[module], 'setup'):
-        setup = 'setup'
-    else:
-        setup = None
-    step = {'module': module,
-            'name': name,
-            'core': testcase['core'],
-            'configuration': testcase['configuration'],
-            'testcase': testcase['name'],
-            'testcase_subdir': testcase['subdir'],
-            'subdir': name,
-            'setup': setup,
-            'run': 'run',
-            'inputs': [],
-            'outputs': []}
-    return step
-
-
-def _get_testcase_default(module):
-    """
-    Set up a default dictionary describing the test case in the given module.
-    The dictionary contains the full name of the python module for the
-    test case, the name of the test case (the final part of the full module
-    name), the default subdirectory for the test case (the same as the
-    ``name``), the names of the ``configure()`` and ``run()`` functions within
-    the module, and the ``core`` and ``configuration`` of the test case (the
-    parsed from the full module name).
-
-    Parameters
-    ----------
-    module : str
-        The full name of the python module for the test case, usually supplied
-        from ``__name__``
-
-    Returns
-    -------
-    testcase : dict
-        A dictionary with the default information about the test case, most of
-        which can be modified as appropriate
-    """
-    name = module.split('.')[-1]
-    core = module.split('.')[1]
-    configuration = module.split('.')[3]
-    subdir = name
-    path = os.path.join(core, configuration, subdir)
-    # some test cases may not need a configure function
-    if hasattr(sys.modules[module], 'configure'):
-        configure = 'configure'
-    else:
-        configure = None
-    testcase = {'module': module,
-                'name': name,
-                'path': path,
-                'core': core,
-                'configuration': configuration,
-                'subdir': subdir,
-                'description': None,
-                'steps': dict(),
-                'configure': configure,
-                'run': 'run',
-                'new_step_log_file': True,
-                'steps_to_run': list()}
-    return testcase
+        if len(missing_files) > 0:
+            raise OSError(
+                'output file(s) missing in step {} of {}/{}/{}: {}'.format(
+                    step.name, step.mpas_core.name, step.test_group.name,
+                    step.test_case.subdir, missing_files))
