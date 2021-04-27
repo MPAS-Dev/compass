b3cae7be2c1dc43c244eab5fe70fbe65362fbccf
Xylar Asay-Davis <xylarstorm@gmail.com>
2021-04-27 20:14:19 +0200

    Detect errors in activation script

--- load/load_compass_env.sh
+++ load/load_compass_env.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 script=$(./load/get_activation_script.py "$@")
+if [ $? -eq 0 ]
+then
+  echo "sourcing: ${script}"
+  source ${script}
+fi
 
-echo "sourcing: ${script}"
-source ${script}
