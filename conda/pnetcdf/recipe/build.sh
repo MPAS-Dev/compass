ce7513103d9c3c0a1a6d1de7b28cd623c9d19132
Xylar Asay-Davis <xylarstorm@gmail.com>
2021-06-04 13:04:37 +0200

    Add a conda recipe for pnetcdf

---
+++ conda/pnetcdf/recipe/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export MPICC=mpicc
+export MPICXX=mpicxx
+export MPIF77=mpifort
+export MPIF90=mpifort
+
+./configure --prefix=${PREFIX} \
+    --with-netcdf4=${PREFIX}
+
+make
+# serial tests
+# make check
+# lighter weight parallel tests (4 MPI tasks)
+# make ptest
+
+make install
