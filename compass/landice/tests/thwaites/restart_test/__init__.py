1ea1c071a36b1214d8382b8673d02f0006c38de6
Matthew Hoffman <mhoffman@lanl.gov>
2021-06-14 14:50:19 -0600

    Add premade Thwaites 4km test group
    This is a simple configuration that downloads a pre-existing 4km mesh
    with an initial condition and then runs basic decomp and restart tests.
    The tests are also added to the test suites.  This test only applies to
    the FO velocity solver, so the velocity solver is not included in the
    name or as a property of the test case.

---
+++ compass/landice/tests/thwaites/restart_test/__init__.py
@@ -0,0 +1,78 @@
+from compass.validate import compare_variables
+from compass.testcase import TestCase
+from compass.landice.tests.thwaites.run_model import RunModel
+
+
+class RestartTest(TestCase):
+    """
+    A test case for performing two MALI runs of the Thwaites setup,
+    one full run and one run broken into two segments with a restart.  The
+    test case verifies that the results of the two runs are identical.
+    """
+
+    def __init__(self, test_group):
+        """
+        Create the test case
+
+        Parameters
+        ----------
+        test_group : compass.landice.tests.thwaites
+            The test group that this test case belongs to
+
+        """
+        name = 'restart_test'
+        subdir = '{}'.format(name)
+        super().__init__(test_group=test_group, name=name, subdir=subdir)
+
+        cores = 36
+        min_cores = 4
+
+        name = 'full_run'
+        step = RunModel(test_case=self, name=name, subdir=name, cores=cores,
+                        min_cores=min_cores, threads=1)
+        # modify the namelist options and streams file
+        step.add_namelist_file(
+            'compass.landice.tests.greenland.restart_test',
+            'namelist.full', out_name='namelist.landice')
+        step.add_streams_file(
+            'compass.landice.tests.greenland.restart_test',
+            'streams.full', out_name='streams.landice')
+        self.add_step(step)
+
+        name = 'restart_run'
+        step = RunModel(test_case=self, name=name, subdir=name, cores=cores,
+                        min_cores=min_cores, threads=1,
+                        suffixes=['landice', 'landice.rst'])
+
+        # modify the namelist options and streams file
+        step.add_namelist_file(
+            'compass.landice.tests.thwaites.restart_test',
+            'namelist.restart', out_name='namelist.landice')
+        step.add_streams_file(
+            'compass.landice.tests.thwaites.restart_test',
+            'streams.restart', out_name='streams.landice')
+
+        step.add_namelist_file(
+            'compass.landice.tests.thwaites.restart_test',
+            'namelist.restart.rst', out_name='namelist.landice.rst')
+        # same streams file for both restart stages
+        step.add_streams_file(
+            'compass.landice.tests.thwaites.restart_test',
+            'streams.restart', out_name='streams.landice.rst')
+        self.add_step(step)
+
+    # no configure() method is needed
+
+    # no run() method is needed
+
+    def validate(self):
+        """
+        Test cases can override this method to perform validation of variables
+        and timers
+        """
+        variables = ['thickness', 'surfaceSpeed']
+        steps = self.steps_to_run
+        if 'full_run' in steps and 'restart_run' in steps:
+            compare_variables(test_case=self, variables=variables,
+                              filename1='full_run/output.nc',
+                              filename2='restart_run/output.nc')
