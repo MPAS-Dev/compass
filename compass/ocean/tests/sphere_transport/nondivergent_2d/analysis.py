0fdae3dd7033b1ea7c6834c06be87cdf30a9e7c8
Yaris Eidenbenz <yeidenbenz@lanl.gov>
2021-10-22 14:55:13 -0700

    add convergence thresholds to correlated nondivergent_2d test

--- compass/ocean/tests/sphere_transport/nondivergent_2d/analysis.py
+++ compass/ocean/tests/sphere_transport/nondivergent_2d/analysis.py
@@ -142,3 +142,13 @@ class Analysis(Step):
             'nondivergent2D_range_filament_err.pdf',
             bbox_inches='tight')
         plt.close(fig)
+
+        section = self.config['nondivergent_2d']
+        conv_thresh = section.getfloat('conv_thresh')
+        conv_max = section.getfloat('conv_max')
+
+        if conv < conv_thresh:
+            raise ValueError(f'order of convergence {conv} < min tolerence {conv_thresh}')
+
+        if conv > conv_max:
+            warnings.warn(f'order of convergence {conv} > max tolerence {conv_max}')
