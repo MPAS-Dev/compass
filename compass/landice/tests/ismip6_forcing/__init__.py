18d510cd0d57d54d211aafe2463b8f6273c945f8
hollyhan <hollyhan@lanl.gov>
2022-04-15 14:59:36 -0600

    Create a testgroup class "Ismip6Forcing"

---
+++ compass/landice/tests/ismip6_forcing/__init__.py
@@ -0,0 +1,19 @@
+from compass.testgroup import TestGroup
+from compass.landice.tests.ismip6_forcing.atmosphere import Atmosphere
+from compass.landice.tests.ismip6_forcing.ocean import Ocean
+
+
+class Ismip6Forcing(TestGroup):
+    """
+    A test group for processing the ISMIP6
+         ocean and atmosphere forcing data
+    """
+    def __init__(self, mpas_core):
+        """
+        mpas_core : compass.landice.Landice
+            the MPAS core that this test group belongs to
+        """
+        super().__init__(mpas_core=mpas_core, name='ismip6_forcing')
+        
+        self.add_test_case(Atmosphere(test_group=self))
+        self.add_test_case(Ocean(test_group=self))
