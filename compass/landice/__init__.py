5ea54bf8fa47ece322c690f762d1149746c7b758
hollyhan <hollyhan@lanl.gov>
2022-04-15 14:57:20 -0600

    Add 'Ismip6Forcing' testcase to the landice core class

--- compass/landice/__init__.py
+++ compass/landice/__init__.py
@@ -8,6 +8,7 @@ from compass.landice.tests.enthalpy_benchmark import EnthalpyBenchmark
 from compass.landice.tests.greenland import Greenland
 from compass.landice.tests.humboldt import Humboldt
 from compass.landice.tests.hydro_radial import HydroRadial
+from compass.landice.tests.ismip6_forcing import Ismip6Forcing
 from compass.landice.tests.kangerlussuaq import Kangerlussuaq
 from compass.landice.tests.koge_bugt_s import KogeBugtS
 from compass.landice.tests.mismipplus import MISMIPplus
@@ -34,6 +35,7 @@ class Landice(MpasCore):
         self.add_test_group(Greenland(mpas_core=self))
         self.add_test_group(Humboldt(mpas_core=self))
         self.add_test_group(HydroRadial(mpas_core=self))
+        self.add_test_group(Ismip6Forcing(mpas_core=self))
         self.add_test_group(Kangerlussuaq(mpas_core=self))
         self.add_test_group(KogeBugtS(mpas_core=self))
         self.add_test_group(MISMIPplus(mpas_core=self))
