Correct basal mass balance sign
Use convention that negative mass balance is melting.

compass/landice/tests/antarctica/mesh.py
@@ -157,8 +157,11 @@ class Mesh(Step):
         data.variables['vErr'][:] = np.sqrt(data.variables['ex'][:]**2
                                             + data.variables['ey'][:]**2)
 
-        data.variables['bheatflx'][:] *= -1.e-3  # correct units
+        data.variables['bheatflx'][:] *= -1.e-3  # correct units and sign
         data.variables['bheatflx'].units = 'W m-2'
+
+        data.variables['subm'][:] *= -1.0  # correct basal melting sign
+        data.variables['subm_ss'][:] *= -1.0 
 
         data.renameVariable('dhdt', 'dHdt')
         data.renameVariable('thkerr', 'topgerr')