02d110bdb2bb4e2a78ccf800994f81b611a3d524
hollyhan <hollyhan@lanl.gov>
2022-05-03 22:05:59 -0600

    Make major edits to the ismip6_forcing/atmosphere testcase
    1. add/get different config options for input and output files and their paths
    2. add a nested dictionary for loading different isimip6 forcing files
    3. add a method that applies the MALI base SMB to the ismip6 smb anomaly field
    4. fix how to add xtime variable; use `xarray.DataArray.dt` instead of `pandas.to_datetime`

--- compass/landice/tests/ismip6_forcing/atmosphere/process_smb.py
+++ compass/landice/tests/ismip6_forcing/atmosphere/process_smb.py
@@ -1,12 +1,8 @@
 import os
-import pandas as pd
-import shutil
 import subprocess
 import xarray as xr
-import numpy as np
 from compass.landice.tests.ismip6_forcing.atmosphere.create_mapfile_smb \
     import build_mapping_file
-from mpas_tools.scrip.from_mpas import scrip_from_mpas
 from mpas_tools.io import write_netcdf
 from compass.step import Step
 
@@ -16,7 +12,7 @@ class ProcessSMB(Step):
     A step for processing the ISMIP6 surface mass balance data
     """
 
-    def __init__(self, test_case):
+    def __init__(self, test_case, input_file=None):
         """
         Create the step
 
@@ -24,7 +20,10 @@ class ProcessSMB(Step):
         ----------
         test_case : compass.landice.tests.ismip6_forcing.atmosphere.Atmosphere
             The test case this step belongs to
+
+        input_file : file name of ismip6 forcing data processed by this step
         """
+        self.input_file = input_file
         super().__init__(test_case=test_case, name='process_smb')
 
     def setup(self):
@@ -32,16 +31,43 @@ class ProcessSMB(Step):
         Set up this step of the test case
         """
         config = self.config
-        section = config['ismip6_ais_atmosphere']
+        section = config['ismip6_ais']
 
-        input_path = section.get('input_path')
-        input_file = section.get('input_file')
-        self.add_input_file(filename=input_file,
-                            target=os.path.join(input_path, input_file))
+        base_path_ismip6 = section.get('base_path_ismip6')
+        base_path_mali = section.get('base_path_mali')
         mali_mesh_file = section.get('mali_mesh_file')
+
         self.add_input_file(filename=mali_mesh_file,
-                            target=os.path.join(input_path, mali_mesh_file))
-        self.add_output_file(filename=f"output_{input_file}")
+                            target=os.path.join(base_path_mali,
+                                                mali_mesh_file))
+
+        section = config['ismip6_ais']
+        period_endyear = section.get("period_endyear")
+        model = section.get("model")
+        scenario = section.get("scenario")
+
+        if period_endyear == "NotAvailable":
+            raise ValueError("You need to supply a user config file, which "
+                             "should contain the ismip6_ais "
+                             "section with the period_endyear option")
+        if model == "NotAvailable":
+            raise ValueError("You need to supply a user config file, which "
+                             "should contain the ismip6_ais "
+                             "section with the model option")
+        if scenario == "NotAvailable":
+            raise ValueError("You need to supply a user config file, which "
+                             "should contain the ismip6_ais "
+                             "section with the scenario option")
+
+        input_file_list = self._files[period_endyear][model][scenario]
+        for file in input_file_list:
+            print(base_path_ismip6)
+            print(os.path.join(base_path_ismip6, file))
+            self.add_input_file(filename=os.path.basename(file),
+                                target=os.path.join(base_path_ismip6, file))
+
+        output_file = f"processed_{model}_{scenario}_{period_endyear}.nc"
+        self.add_output_file(filename=output_file)
 
     def run(self):
         """
@@ -49,32 +75,55 @@ class ProcessSMB(Step):
         """
         logger = self.logger
         config = self.config
-        section = config['ismip6_ais_atmosphere']
 
-        input_file = section.get('input_file')
+        section = config['ismip6_ais']
+        # base_path_ismip6 = section.get('base_path_ismip6')
         mali_mesh_name = section.get('mali_mesh_name')
         mali_mesh_file = section.get('mali_mesh_file')
+        period_endyear = section.get("period_endyear")
+        model = section.get("model")
+        scenario = section.get("scenario")
+
+        section = config['ismip6_ais_atmosphere']
         method_remap = section.get('method_remap')
-        output_file = f"output_{input_file}"
+
+        input_file_list = self._files[period_endyear][model][scenario]
+        i = 0
+        for file in input_file_list:
+            input_file_list[i] = os.path.basename(file)
+            i += 1
+
+        input_file_combined = xr.open_mfdataset(input_file_list,
+                                                concat_dim='time',
+                                                combine='nested',
+                                                engine='netcdf4')
+        combined_file_temp = "combined.nc"
+        write_netcdf(input_file_combined, combined_file_temp)
 
         # interpolate and rename the ismip6 thermal forcing data
         remapped_file_temp = "remapped.nc"  # temporary file name
 
         # call the function that reads in, remap and rename the file.
         logger.info("Calling a remapping function...")
-        self.remap_ismip6smb_to_mali(input_file, remapped_file_temp,
+        self.remap_ismip6smb_to_mali(combined_file_temp, remapped_file_temp,
                                      mali_mesh_name, mali_mesh_file,
                                      method_remap)
 
         # call the function that renames the ismip6 variables to MALI variables
         logger.info("Renaming the ismip6 variables to mali variable names...")
+        output_file = f"processed_{model}_{scenario}_{period_endyear}.nc"
         self.rename_ismip6smb_to_mali_vars(remapped_file_temp, output_file)
 
-        logger.info("Remapping and renamping process done successfully. "
-                    "Removing the temporary file 'remapped.nc'")
+        # correct the SMB anomaly field with mali base SMB field
+        logger.info("Correcting the SMB anomaly field with MALI base SMB...")
+        self.correct_SMB_anomaly_for_baseSMB(output_file, mali_mesh_file)
 
-        # remove the temporary combined file
+        logger.info("Processing done successfully. "
+                    "Removing the temporary files 'remapped.nc' and "
+                    "'combined.nc'...")
+        # remove the temporary remapped and combined files
         os.remove(remapped_file_temp)
+        os.remove(combined_file_temp)
 
     def remap_ismip6smb_to_mali(self, input_file, output_file, mali_mesh_name,
                                 mali_mesh_file, method_remap):
@@ -95,7 +144,7 @@ class ProcessSMB(Step):
             Remapping method used in building a mapping file
         """
 
-        # check if a mapfile
+        # check if a mapfile exists
         mapping_file = f"map_ismip6_8km_to_{mali_mesh_name}_{method_remap}.nc"
 
         if not os.path.exists(mapping_file):
@@ -145,9 +194,9 @@ class ProcessSMB(Step):
         # add xtime variable
         xtime = []
         for t_index in range(ds.sizes["Time"]):
-            date = ds.Time[t_index].values
-            date = pd.to_datetime(str(date))
-            date = date.strftime("%Y-%m-%d_00:00:00").ljust(64)
+            date = ds.Time[t_index]
+            date = date.dt.strftime("%Y-%m-%d_00:00:00")
+            date = str(date.values).ljust(64)
             xtime.append(date)
 
         ds["xtime"] = ("Time", xtime)
@@ -160,3 +209,122 @@ class ProcessSMB(Step):
         # write to a new netCDF file
         write_netcdf(ds, output_file)
         ds.close()
+
+    def correct_SMB_anomaly_for_baseSMB(self, output_file, mali_mesh_file):
+        """
+        Apply the MALI base SMB to the ismip6 SMB anomaly field
+
+        Parameters
+        ----------
+        output_file : str
+            remapped ismip6 data renamed on mali mesh
+        mali_mesh_file : str
+            initialized MALI mesh file in which the base SMB field exists
+        """
+
+        ds = xr.open_dataset(output_file)
+        ds_base = xr.open_dataset(mali_mesh_file)
+        for i in range(len(ds["sfcMassBal"])):
+            ds["sfcMassBal"][i] = ds["sfcMassBal"][i] \
+                                  + ds_base["sfcMassBal"][0,:]
+
+        # write to a new netCDF file
+        write_netcdf(ds, output_file)
+        ds.close()
+
+    # create a nested dictionary for the ISMIP6 original forcing files including relative path
+    _files = {
+        "2100": {
+            "CCSM4": {
+                "RCP26": ["AIS/Atmosphere_forcing/ccsm4_rcp2.6/Regridded_8km/CCSM4_8km_anomaly_rcp26_1995-2100.nc"],
+                "RCP85": ["AIS/Atmosphere_Forcing/ccsm4_rcp8.5/Regridded_8km/CCSM4_8km_anomaly_1995-2100.nc"]
+            },
+            "CESM2": {
+                "SSP585v1": [
+                    "AIS/Atmosphere_Forcing/CESM2_ssp585/Regridded_8km/CESM2_anomaly_ssp585_1995-2100_8km_v1.nc"],
+                "SSP585v2": [
+                    "AIS/Atmosphere_forcing/CESM2_ssp585/Regridded_8km/CESM2_anomaly_ssp585_1995-2100_8km_v2.nc"]
+            },
+            "CNRM_CM6": {
+                "SSP126": [
+                    "AIS/Atmosphere_forcing/CNRM_CM6_ssp126/Regridded_8km/CNRM-CM6-1_anomaly_ssp126_1995-2100_8km_ISMIP6.nc"],
+                "SSP585": [
+                    "AIS/Atmosphere_forcing/CNRM_CM6_ssp585/Regridded_8km/CNRM-CM6-1_anomaly_ssp585_1995-2100_8km_ISMIP6.nc"]
+            },
+            "CNRM_ESM2": {
+                "SSP585": [
+                    "AIS/Atmosphere_forcing/CNRM_ESM2_ssp585/Regridded_8km/CNRM-ESM2-1_anomaly_ssp585_1995-2100_8km_ISMIP6.nc"]
+            },
+            "CSIRO-Mk3-6-0": {
+                "RCP85": [
+                    "AIS/Atmosphere_forcing/CSIRO-Mk3-6-0_rcp85/Regridded_8km/CSIRO-Mk3-6-0_8km_anomaly_rcp85_1995-2100.nc"]
+            },
+            "HadGEM2-ES": {
+                "RCP85": [
+                    "AIS/Atmosphere_forcing/HadGEM2-ES_rcp85/Regridded_8km/HadGEM2-ES_8km_anomaly_rcp85_1995-2100.nc"]
+            },
+            "IPSL-CM5A-MR": {
+                "RCP26": [
+                    "AIS/Atmosphere_Forcing/IPSL-CM5A-MR_rcp26/Regridded_8km/IPSL-CM5A-MR_8km_anomaly_rcp26_1995-2100.nc"],
+                "RCP85": [
+                    "AIS/Atmosphere_Forcing/IPSL-CM5A-MR_rcp85/Regridded_8km/IPSL-CM5A-MR_8km_anomaly_rcp85_1995-2100.nc"]
+            },
+            "MIROC-ESM-CHEM": {
+                "RCP26": [
+                    "AIS/Atmosphere_Forcing/miroc-esm-chem_rcp2.6/Regridded_8km/MIROC-ESM-CHEM_8km_anomaly_rcp26_1995-2100.nc"],
+                "RCP85": [
+                    "AIS/Atmosphere_Forcing/miroc-esm-chem_rcp8.5/Regridded_8km/MIROC-ESM-CHEM_8km_anomaly_1995-2100.nc"]
+            },
+            "NorESM1-M": {
+                "RCP26": [
+                    "AIS/Atmosphere_Forcing/noresm1-m_rcp2.6/Regridded_8km/NorESM-M_8km_anomaly_rcp26_1995-2100.nc"],
+                "RCP85": ["AIS/Atmosphere_Forcing/noresm1-m_rcp8.5/Regridded_8km/NorESM-M_8km_anomaly_1995-2100.nc"]
+            },
+            "UKESM1-0-LL": {
+                "SSP585": [
+                    "AIS/Atmosphere_Forcing/UKESM1-0-LL/Regridded_8km/UKESM1-0-LL_anomaly_ssp585_1995-2100_8km.nc"]
+            }
+        },
+        "2300": {
+            "CCSM4": {
+                "RCP85": ["AIS/Atmospheric_forcing/CCSM4_RCP85/Regridded_08km/CCSM4_8km_anomaly_1995-2100.nc",
+                          "AIS/Atmospheric_forcing/CCSM4_RCP85/Regridded_08km/CCSM4_8km_anomaly_2101-2300.nc"]
+            },
+            "CESM2-WACCM": {
+                "SSP585": [
+                    "AIS/Atmospheric_forcing/CESM2-WACCM_ssp585/Regridded_8km/CESM2-WACCM_8km_anomaly_ssp585_1995-2100.nc",
+                    "AIS/Atmospheric_forcing/CESM2-WACCM_ssp585/Regridded_8km/CESM2-WACCM_8km_anomaly_ssp585_2101-2299.nc"],
+                "SSP585-repeat": [
+                    "AIS/Atmospheric_forcing/CESM2-WACCM_ssp585-repeat/Regridded_8km/CESM2-WACCM_8km_anomaly_ssp585_1995-2300.nc"]
+            },
+            "CSIRO-Mk3-6-0": {
+                "RCP85": [
+                    "AIS/Atmospheric_forcing/CSIRO-Mk3-6-0_RCP85/Regridded_8km/CSIRO-Mk3-6-0_8km_anomaly_rcp85_1995-2100.nc",
+                    "AIS/Atmospheric_forcing/CSIRO-Mk3-6-0_RCP85/Regridded_8km/CSIRO-Mk3-6-0_8km_anomaly_rcp85_2101-2300.nc"]
+            },
+            "HadGEM2-ES": {
+                "RCP85": [
+                    "AIS/Atmospheric_forcing/HadGEM2-ES_RCP85/Regridded_8km/HadGEM2-ES_8km_anomaly_rcp85_1995-2100.nc",
+                    "AIS/Atmospheric_forcing/HadGEM2-ES_RCP85/Regridded_8km/HadGEM2-ES_8km_anomaly_rcp85_2101-2299.nc"],
+                "RCP85-repeat": [
+                    "AIS/Atmospheric_forcing/HadGEM2-ES-RCP85-repeat/Regridded_8km/HadGEM2-ES_8km_anomaly_rcp85_1995-2300.nc"]
+            },
+            "NorESM1-M": {
+                "RCP26-repeat": [
+                    "AIS/Atmospheric_forcing/NorESM1-M_RCP26-repeat/Regridded_8km/NorESM1-M_8km_anomaly_rcp26_1995-2300.nc"],
+                "RCP85-repeat": [
+                    "AIS/Atmospheric_forcing/NorESM1-M_RCP85-repeat/Regridded_8km/NorESM1-M_anomaly_1995-2300.nc"]
+
+            },
+            "UKESM1-0-LL": {
+                "SSP126": [
+                    "AIS/Atmospheric_forcing/UKESM1-0-LL_ssp126/Regridded_8km/UKESM1-0-LL_8km_anomaly_ssp126_1995-2100.nc"
+                    "AIS/Atmospheric_forcing/UKESM1-0-LL_ssp126/Regridded_8km/UKESM1-0-LL_8km_anomaly_ssp126_2101-2300.nc"],
+                "SSP585": [
+                    "AIS/Atmospheric_forcing/UKESM1-0-LL_ssp585/Regridding_8km/UKESM1-0-LL_8km_anomaly_ssp585_1995-2100.nc",
+                    "AIS/Atmospheric_forcing/UKESM1-0-LL_ssp585/Regridding_8km/UKESM1-0-LL_8km_anomaly_ssp585_2101-2300.nc"],
+                "SSP585-repeat": [
+                    "AIS/Atmospheric_forcing/UKESM1-0-LL_ssp585-repeat/Regridding_8km/UKESM1-0-LL_8km_anomaly_ssp585_1995-2300.nc"]
+            }
+        }
+    }
