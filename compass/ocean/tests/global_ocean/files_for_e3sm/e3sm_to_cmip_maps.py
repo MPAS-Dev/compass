230e8e570c4bfdf2b7109e177a901308dd92c9c5
Xylar Asay-Davis <xylarstorm@gmail.com>
2022-10-08 15:30:48 -0600

    Add E3smToCmipMaps step
    This computes mapping files needed by e3sm_to_cmip for a given
    mesh.

---
+++ compass/ocean/tests/global_ocean/files_for_e3sm/e3sm_to_cmip_maps.py
@@ -0,0 +1,80 @@
+import os
+import xarray
+
+from mpas_tools.scrip.from_mpas import scrip_from_mpas
+
+from compass.io import symlink
+from compass.step import Step
+
+
+class E3smToCmipMaps(Step):
+    """
+    A step for creating mapping files from the MPAS-Ocean mesh to a standard
+    CMIP6 mesh
+    """
+    def __init__(self, test_case, restart_filename):
+        """
+        Create a new step
+
+        Parameters
+        ----------
+        test_case : compass.ocean.tests.global_ocean.files_for_e3sm.FilesForE3SM
+            The test case this step belongs to
+
+        restart_filename : str
+            A restart file from the end of the dynamic adjustment test case to
+            use as the basis for an E3SM initial condition
+        """
+
+        super().__init__(test_case, name='e3sm_to_cmip_map', ntasks=1,
+                         min_tasks=1, openmp_threads=1)
+
+        self.add_input_file(filename='README', target='../README')
+        self.add_input_file(filename='restart.nc',
+                            target=f'../{restart_filename}')
+
+        self.add_input_file(filename='../scrip/ocean.scrip.nc')
+
+        self.add_input_file(
+            filename='cmip6_180x360_scrip.20181001.nc',
+            target='cmip6_180x360_scrip.20181001.nc',
+            database='map_database')
+
+        self.add_output_file(filename='map_mpas_to_cmip6_180x360_aave.nc')
+        self.add_output_file(filename='map_mpas_to_cmip6_180x360_mono.nc')
+        self.add_output_file(filename='map_mpas_to_cmip6_180x360_nco.nc')
+
+    def run(self):
+        """
+        Run this step of the testcase
+        """
+        with xarray.open_dataset('restart.nc') as ds:
+            mesh_short_name = ds.attrs['MPAS_Mesh_Short_Name']
+            mesh_prefix = ds.attrs['MPAS_Mesh_Prefix']
+            prefix = f'MPAS_Mesh_{mesh_prefix}'
+            creation_date = ds.attrs[f'{prefix}_Version_Creation_Date']
+
+        link_dir = f'../assembled_files/diagnostics/e3sm_to_cmip/maps'
+
+        try:
+            os.makedirs(link_dir)
+        except OSError:
+            pass
+
+        src_scrip_filename = 'ocean.scrip.nc'
+        # Todo: the resolution should be a config option
+        dst_scrip_filename = 'cmip6_180x360_scrip.20181001.nc'
+        map_methods = dict(aave='conserve', mono='fv2fv_flx', nco='nco')
+        for suffix, map_method in map_methods.items():
+            local_map_filename = f'map_mpas_to_cmip6_180x360_{suffix}.nc'
+            args = ['ncremap', f'--alg_typ={map_method}',
+                    f'--grd_src={src_scrip_filename}',
+                    f'--grd_dst={dst_scrip_filename}',
+                    f'--map={local_map_filename}']
+            check_call(args, logger=logger)
+
+            map_filename = \
+                f'map_{mesh_short_name}_to_cmip6_180x360_{suffix}.{creation_date}.nc'
+
+            symlink(f'../../../../e3sm_to_cmip_map/{local_map_filename}',
+                    f'{link_dir}/{map_filename}')
