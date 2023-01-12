9d5bd375a1cb14efef91a50cffd22d4e7138f1f7
Carolyn Begeman <cbegeman@lanl.gov>
2023-01-12 16:42:59 -0600

    cleanup __init__.py

--- compass/ocean/tests/overflow/__init__.py
+++ compass/ocean/tests/overflow/__init__.py
@@ -1,21 +1,23 @@
 from compass.testgroup import TestGroup
 from compass.ocean.tests.overflow.default import Default
 from compass.ocean.tests.overflow.rpe_test import RpeTest
-                                                         
-class Overflow(TestGroup):                               
-    """                                                  
+
+
+class Overflow(TestGroup):
+    """
     A test group for General Ocean Turbulence Model (GOTM) test cases
     """
 
-    def __init__(self, mpas_core):                       
+    def __init__(self, mpas_core):
         """
-        mpas_core : compass.MpasCore                     
+        mpas_core : compass.MpasCore
             the MPAS core that this test group belongs to
         """
         super().__init__(mpas_core=mpas_core, name='overflow')
-        
+
         self.add_test_case(Default(test_group=self, resolution='10km'))
         self.add_test_case(RpeTest(test_group=self, resolution='2km'))
+
 
 def configure(resolution, config):
     """
@@ -29,8 +31,8 @@ def configure(resolution, config):
     config : compass.config.CompassConfigParser
         Configuration options for this test case
     """
-    width = 40 # km
-    length = 200 # km
+    width = 40    # km
+    length = 200  # km
     dc = float(resolution[:-2])
     nx = int(width/dc)
     ny = int(length/dc)
