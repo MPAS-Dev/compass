f0d2feeb8875445562fb20b8ccf9cfdc24e503a1
Matthew Hoffman <mhoffman@lanl.gov>
2023-01-23 16:28:39 -0800

    Switch the spd lim param to basal friction exponent
    This includes:
    * updating parameter vector
    * modifying muFriction for updated exponent.  This makes use of a new general
    extrapolate module added at the shared landice level.  It also switches to
    first copying the input file to the run work_dir so that it can be modified
    in place.  The input file path is now specified in the config file, though we
    may want to move it to the database.
    * modifying exponent value in yaml file.  This required directly copying the
    yaml file instead of using 'add_input_file' so that the yaml contents could
    be modified within the setup method.  It also makes use of a yaml parser to
    modify an entry.  The yaml parser changes the ordering, which might not be desired.
    * Commenting a hanging level of the yaml file, as the yaml parser writes its
    value out as Null, which Albany does not like.
    
    Note: we may add the calving speed limit back in later, but for now we want to
    restrict ourselves to two parameters.

--- compass/landice/tests/thwaites/uq_ensemble/ensemble_member.py
+++ compass/landice/tests/thwaites/uq_ensemble/ensemble_member.py
@@ -3,9 +3,14 @@ from compass.model import make_graph_file, run_model
 from compass.job import write_job_script
 from compass.io import symlink
 from importlib import resources
+from compass.landice.extrapolate import extrapolate_variable
 
 import numpy as np
 import os
+import shutil
+import subprocess
+import yaml
+import netCDF4
 
 class EnsembleMember(Step):
     """
@@ -59,11 +64,23 @@ class EnsembleMember(Step):
         self.add_streams_file(
             'compass.landice.tests.thwaites.uq_ensemble', 'streams.landice')
 
-        self.add_input_file(filename='albany_input.yaml',
-                        package='compass.landice.tests.thwaites.uq_ensemble',
-                        copy=True)
+        # copy over albany yaml file
+        # cannot use add_input functionality because we need to modify the file
+        # in this function, and inputs don't get processed until after this
+        # function
+        with resources.path('compass.landice.tests.thwaites.uq_ensemble',
+                            'albany_input.yaml') as package_path:
+            target = str(package_path)
+            shutil.copy(target, self.work_dir)
 
         self.add_model_as_input()
+
+        # copy in input file so it can be modified
+        config = self.config
+        section = config['thwaites_uq']
+        input_file_path = section.get('input_file_path')
+        shutil.copy(input_file_path, self.work_dir)
+        input_file_name = input_file_path.split('/')[-1]
 
         # modify param values as needed for this ensemble member
 
@@ -71,9 +88,9 @@ class EnsembleMember(Step):
         param_file_name = self.config.get('thwaites_uq',
                                           'param_vector_filename')
         with resources.open_text('compass.landice.tests.thwaites.uq_ensemble', param_file_name) as params_file:
-            vm_thresh_vec, gamma0_vec = np.loadtxt(params_file, delimiter=',',
-                                                   skiprows=1,
-                                                   usecols=(1,2), unpack=True)
+            vm_thresh_vec, fric_exp_vec = np.loadtxt(params_file, delimiter=',',
+                                                     skiprows=1,
+                                                     usecols=(1,2), unpack=True)
 
         # von Mises stress threshold
         #self.vM_value = np.random.uniform(150.0e3, 400.0e3)
@@ -84,14 +101,20 @@ class EnsembleMember(Step):
                    f'{self.vM_value}'}
 
         # von Mises speed limit
-        secYr = 3600.0 * 24.0 * 365.0
-        self.clv_spd_lim_value = np.random.uniform(8000.0, 30000.0)
-        options['config_calving_speed_limit'] = \
-                f'{self.clv_spd_lim_value / secYr}'
+        #secYr = 3600.0 * 24.0 * 365.0
+        #self.clv_spd_lim_value = np.random.uniform(8000.0, 30000.0)
+        #options['config_calving_speed_limit'] = \
+        #        f'{self.clv_spd_lim_value / secYr}'
 
         self.add_namelist_options(options=options,
                                   out_name='namelist.landice')
 
+        # adjust basal friction exponent
+        orig_fric_exp = 0.2  # set by initial condition file being used
+        _adjust_friction_exponent(orig_fric_exp, float(fric_exp_vec[self.run_num]),
+                                  os.path.join(self.work_dir, input_file_name),
+                                  os.path.join(self.work_dir,
+                                               'albany_input.yaml'))
 
         # set job name to run number so it will get set in batch script
         self.config.set('job', 'job_name', f'uq_{self.name}')
@@ -117,3 +140,33 @@ class EnsembleMember(Step):
         make_graph_file(mesh_filename='/global/project/projectdirs/piscees/MALI_projects/Thwaites_UQ/thwaites_4km_mesh_20221122/input_files/Thwaites.nc',
                         graph_filename='graph.info')
         run_model(self)
+
+
+def _adjust_friction_exponent(orig_fric_exp, new_fric_exp, filename, albany_input_yaml):
+    f = netCDF4.Dataset(filename, 'r+')
+    mu = f.variables['muFriction'][0,:]
+    uX = f.variables['uReconstructX'][0,:,:]
+    uY = f.variables['uReconstructY'][0,:,:]
+    spd = (uX[:,-1]**2 + uY[:,-1]**2)**0.5 * (60.*60.*24.*365.)
+    exp_adjustment = orig_fric_exp - new_fric_exp
+    mu = mu * spd**exp_adjustment
+    mu[spd == 0.0] = 0.0  # The previous step leads to infs or nans in ice-free areas. Set them all to 0.0 for the extrapolation step
+    f.variables['muFriction'][0,:] = mu
+    f.close()
+
+    extrapolate_variable(filename, 'muFriction', 'min')
+
+    # now set exp in albany yaml file
+    with open(albany_input_yaml, 'r') as stream:
+        try:
+            loaded = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+    # Change value
+    loaded['ANONYMOUS']['Problem']['LandIce BCs']['BC 0']['Basal Friction Coefficient']['Power Exponent'] = new_fric_exp
+    # write out again
+    with open(albany_input_yaml, 'w') as stream:
+        try:
+            yaml.dump(loaded, stream, default_flow_style=False)
+        except yaml.YAMLError as exc:
+            print(exc)
