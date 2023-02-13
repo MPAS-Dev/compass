8566f5201a70d1c48d41326559d3903f93c21870
Matthew Hoffman <mhoffman@lanl.gov>
2023-02-12 19:53:47 -0800

    Make list of experiments to set up defined in cfg

--- compass/landice/tests/ismip6_run_ais/projection/__init__.py
+++ compass/landice/tests/ismip6_run_ais/projection/__init__.py
@@ -3,6 +3,8 @@ from compass.landice.tests.ismip6_run_ais.set_up_experiment \
         import SetUpExperiment
 from compass.validate import compare_variables
 
+import os
+
 
 class Projection(TestCase):
     """
@@ -24,26 +26,41 @@ class Projection(TestCase):
         super().__init__(test_group=test_group, name=name,
                          subdir=name)
 
-        # tier 1
-        experiments = ['hist', 'ctrlAE', 'expAE01', 'expAE02', 'expAE03',
-                       'expAE04', 'expAE05', 'expAE06']
-        # tier 2
-        for i in range(7, 14+1):
-            experiments.append(f'expAE{i:02}')
-
-        for exp in experiments:
-            name = f'{exp}'
-            self.add_step(
-               SetUpExperiment(test_case=self, name=name,
-                              subdir=f"{exp}", exp=exp))
-
-    def validate(self):
+    def configure(self):
         """
-        Test cases can override this method to perform validation of variables
-        and timers
+        Set up the desired ISMIP6 experiments.
+
+        Read the list from the config of which experiemnts the
+        user wants to set up.  Call thee add_step method and add the
+        experiment to steps_to_run.  Those operations are typically done
+        in the constructor, but they are done here so that the list to set up
+        can be adjusted in the config, and the config is not available until
+        this point.
         """
-        var_list = ['thickness']
-        filename = f'{self.steps["run_model"].subdir}/output.nc'
-        compare_variables(test_case=self,
-                          variables=var_list,
-                          filename1=filename)
+        exp_list = self.config.get('ismip6_run_ais', 'exp_list')
+        if exp_list == "tier1":
+            exp_list = ['hist', 'ctrlAE'] + \
+                       [f'expAE{i:02}' for i in range(1, 7)]
+        elif exp_list == "tier2":
+            exp_list = [f'expAE{i:02}' for i in range(7, 15)]
+        elif exp_list == "all":
+            exp_list = ['hist', 'ctrlAE'] + \
+                       [f'expAE{i:02}' for i in range(1, 15)]
+        else:
+            exp_list = exp_list.split(",")
+
+        for exp in exp_list:
+            if os.path.exists(os.path.join(self.work_dir, exp)):
+                print(f"WARNING: {exp} path already exists; skipping.  "
+                      "Please remove the directory "
+                      f"{os.path.join(self.work_dir, exp)} and execute "
+                      "'compass setup' again to set this experiment up.")
+            else:
+                self.add_step(
+                    SetUpExperiment(test_case=self, name=exp,
+                                    subdir=exp, exp=exp))
+                self.steps_to_run.append(exp)
+
+    # no run() method is needed
+
+    # no validate() method is needed
