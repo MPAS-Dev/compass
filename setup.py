35e44c3e9751ec1e8c0527a40a3075c354790d61
Matthew Hoffman <mhoffman@lanl.gov>
2023-02-23 15:00:46 -0800

    One more piece of lint

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
 import os
-import re
-from setuptools import setup, find_packages
+
+from setuptools import find_packages, setup
 
 
 def package_files(directory, prefixes, extensions):
