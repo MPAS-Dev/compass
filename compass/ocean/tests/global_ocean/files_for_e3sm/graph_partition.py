b51a0329446bbcecf23fc0432c305a71ae934d1f
Xylar Asay-Davis <xylarstorm@gmail.com>
2023-03-13 23:55:11 +0100

    Update algo. for MPAS possible MPAS partition cores
    The old algorithm wasn't generating the breadth of parition
    sizes that the E3SM team needs.  The updated algorithm seems
    likely to generate nearly all of the partition sizes that
    could conceivable be useful for a given mesh.

--- compass/ocean/tests/global_ocean/files_for_e3sm/graph_partition.py
+++ compass/ocean/tests/global_ocean/files_for_e3sm/graph_partition.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def get_core_list(ncells, max_cells_per_core=6000, min_cells_per_core=100):
+def get_core_list(ncells, max_cells_per_core=30000, min_cells_per_core=2):
     """
     Get a fairly exhaustive list of core counts to partition a given number of
     cells into
@@ -24,17 +24,36 @@ def get_core_list(ncells, max_cells_per_core=6000, min_cells_per_core=100):
     cores : numpy.ndarray
         Likely numbers of cores to run with
     """
-    min_graph_size = int(ncells / max_cells_per_core)
+    min_graph_size = max(2, int(ncells / max_cells_per_core))
     max_graph_size = int(ncells / min_cells_per_core)
-    n_power2 = 2**np.arange(1, 21)
-    n_multiples12 = 12 * np.arange(1, 9)
 
-    cores = n_power2
-    for power10 in range(3):
-        cores = np.concatenate([cores, 10**power10 * n_multiples12])
+    cores = []
+    for candidate in range(min_graph_size, max_graph_size):
+        factors = _prime_factors(candidate)
+        twos = np.count_nonzero(factors == 2)
+        fives = np.count_nonzero(factors == 5)
+        gt_five = np.count_nonzero(factors > 5)
+        big_factor = factors.max()
+        if twos > 0 and fives <= twos and gt_five <= 1 and big_factor <= 7:
+            cores.append(candidate)
+        # small odd multiples of 3 and a few that correspond to divisors of the
+        # ne30 (30x30x6=5400) size
+        elif candidate in [3, 9, 15, 21, 225, 675, 1350]:
+            cores.append(candidate)
 
-    mask = np.logical_and(cores >= min_graph_size,
-                          cores <= max_graph_size)
-    cores = cores[mask]
+    return np.array(cores)
 
-    return cores
+
+# https://stackoverflow.com/a/22808285
+def _prime_factors(n):
+    i = 2
+    factors = []
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            factors.append(i)
+    if n > 1:
+        factors.append(n)
+    return np.array(factors)
