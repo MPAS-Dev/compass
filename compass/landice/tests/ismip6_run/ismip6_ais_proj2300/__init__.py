a7ec870d380f323a048024ee6ffaa80ac1a82010
Matthew Hoffman <mhoffman@lanl.gov>
2023-03-13 10:18:55 -0700

    Major reorganization
    Make testgroup=ismip6_run, testcase=ismip6_ais_proj2300.
    
    This will allow other ISMIP6 experiments (e.g. Greenland) to be other
    test cases in this test group.  I moved all modules and resources into
    the testcase level.  If other testcases are added, it may be possible to
    generalize some pieces and move them to the testgroup level, but it also
    may turn out easier to have dedicated version for each ensemble.

--- compass/landice/tests/ismip6_run/ismip6_ais_proj2300/__init__.py
+++ compass/landice/tests/ismip6_run/ismip6_ais_proj2300/__init__.py
@@ -1,14 +1,16 @@
-from compass.testcase import TestCase
-from compass.landice.tests.ismip6_run_ais.set_up_experiment \
-        import SetUpExperiment
-from compass.validate import compare_variables
-
 import os
 
+from compass.landice.tests.ismip6_run.ismip6_ais_proj2300.set_up_experiment import (  # noqa
+    SetUpExperiment,
+)
+from compass.testcase import TestCase
 
-class Projection(TestCase):
+
+class Ismip6AisProj2300(TestCase):
     """
-    A test case for performing forward MALI runs of ISMIP6 Antaractic setup
+    A test case for automated setup of a suite of standardized
+    simulations for ISMIP6-Projections2300-Antarctica
+    See: https://www.climate-cryosphere.org/wiki/index.php?title=ISMIP6-Projections2300-Antarctica)  # noqa
     """
 
     def __init__(self, test_group):
@@ -17,18 +19,18 @@ class Projection(TestCase):
 
         Parameters
         ----------
-        test_group : compass.landice.tests.ismip6_run_ais.Ismip6RunAIS
+        test_group : compass.landice.tests.ismip6_run.Ismip6Run
             The test group that this test case belongs to
 
         """
-        name = 'ismip6AE'
+        name = 'ismip6_ais_proj2300'
 
         super().__init__(test_group=test_group, name=name,
                          subdir=name)
 
     def configure(self):
         """
-        Set up the desired ISMIP6 experiments.
+        Set up the desired ISMIP6 AIS 2300 experiments.
 
         Read the list from the config of which experiemnts the
         user wants to set up.  Call thee add_step method and add the
@@ -37,7 +39,10 @@ class Projection(TestCase):
         can be adjusted in the config, and the config is not available until
         this point.
         """
-        exp_list = self.config.get('ismip6_run_ais', 'exp_list')
+
+        # user can specify any of: 'all', 'tier1', 'tier2', or a
+        # comma-delimited list (or a single experiment)
+        exp_list = self.config.get('ismip6_run_ais_2300', 'exp_list')
         if exp_list == "tier1":
             exp_list = ['hist', 'ctrlAE'] + \
                        [f'expAE{i:02}' for i in range(1, 7)]
@@ -59,7 +64,6 @@ class Projection(TestCase):
                 self.add_step(
                     SetUpExperiment(test_case=self, name=exp,
                                     subdir=exp, exp=exp))
-                self.steps_to_run.append(exp)
 
     # no run() method is needed
 
--- compass/landice/tests/ismip6_run/ismip6_ais_proj2300/set_up_experiment.py
+++ compass/landice/tests/ismip6_run/ismip6_ais_proj2300/set_up_experiment.py
@@ -1,9 +1,12 @@
+import glob
 import os
-from compass.model import run_model, make_graph_file
-from compass.step import Step
-import shutil, glob, sys
-from jinja2 import Template
+import shutil
+import sys
 from importlib import resources
+
+from jinja2 import Template
+
+from compass.step import Step
 
 
 class SetUpExperiment(Step):
@@ -28,12 +31,12 @@ class SetUpExperiment(Step):
 
         super().__init__(test_case=test_case, name=name, subdir=subdir)
 
-    def setup(self):
+    def setup(self):  # noqa:C901
 
         print(f"    Setting up experiment {self.exp}")
 
         config = self.config
-        section = config['ismip6_run_ais']
+        section = config['ismip6_run_ais_2300']
         mesh_res = section.getint('mesh_res')
         forcing_basepath = section.get('forcing_basepath')
         init_cond_path = section.get('init_cond_path')
@@ -42,6 +45,7 @@ class SetUpExperiment(Step):
         melt_params_fname = os.path.split(melt_params_path)[-1]
         region_mask_path = section.get('region_mask_path')
         region_mask_fname = os.path.split(region_mask_path)[-1]
+        calving_method = section.get('calving_method')
         graph_files_path = section.get('graph_files_path')
 
         if self.exp == 'hist':
@@ -49,20 +53,28 @@ class SetUpExperiment(Step):
         else:
             exp_fcg = self.exp
 
-        # We chose to use vM calving at 4km but restore calving at 8km
-        if mesh_res == 4:
+        # Define where to get namelist, streams, yaml, etc. templates
+        # (in current package)
+        resource_location = \
+            'compass.landice.tests.ismip6_run.ismip6_ais_proj2300'
+
+        # Define calving method
+        if calving_method == 'von_mises':
             use_vM_calving = True
-        else:
+        elif calving_method == 'restore':
             use_vM_calving = False
 
         # Figure out if the forcing is in tier1 or tier2 subdir
         if 'exp' in self.exp:
             if int(self.exp[-2:]) >= 7:
-                forcing_basepath = os.path.join(forcing_basepath, 'tier2_experiments')
+                forcing_basepath = os.path.join(forcing_basepath,
+                                                'tier2_experiments')
             else:
-                forcing_basepath = os.path.join(forcing_basepath, 'tier1_experiments')
+                forcing_basepath = os.path.join(forcing_basepath,
+                                                'tier1_experiments')
         else:
-            forcing_basepath = os.path.join(forcing_basepath, 'tier1_experiments')
+            forcing_basepath = os.path.join(forcing_basepath,
+                                            'tier1_experiments')
 
         # Copy files we'll need from local paths specified in cfg file
         if self.exp == 'hist':
@@ -71,30 +83,37 @@ class SetUpExperiment(Step):
         shutil.copy(region_mask_path, self.work_dir)
 
         # Find and copy correct forcing files
-        smb_search_path = os.path.join(forcing_basepath, exp_fcg, 'processed_SMB_*_smbNeg_over_bareland.nc')
+        smb_search_path = os.path.join(
+            forcing_basepath, exp_fcg,
+            'processed_SMB_*_smbNeg_over_bareland.nc')
         fcgFileList = glob.glob(smb_search_path)
         if len(fcgFileList) == 1:
             smb_path = fcgFileList[0]
             smb_fname = os.path.split(smb_path)[-1]
             shutil.copy(smb_path, self.work_dir)
         else:
-            sys.exit(f"ERROR: Did not find exactly one matching SMB file at {smb_search_path}: {fcgFileList}")
+            sys.exit("ERROR: Did not find exactly one matching SMB file at "
+                     f"{smb_search_path}: {fcgFileList}")
 
-        tf_search_path = os.path.join(forcing_basepath, exp_fcg, 'processed_TF_*.nc')
+        tf_search_path = os.path.join(forcing_basepath, exp_fcg,
+                                      'processed_TF_*.nc')
         fcgFileList = glob.glob(tf_search_path)
         if len(fcgFileList) == 1:
             tf_path = fcgFileList[0]
             tf_fname = os.path.split(tf_path)[-1]
             shutil.copy(tf_path, self.work_dir)
         else:
-            sys.exit(f"ERROR: Did not find exactly one matching TF file at {tf_search_path}: {fcgFileList}")
+            sys.exit("ERROR: Did not find exactly one matching TF file at "
+                     f"{tf_search_path}: {fcgFileList}")
 
         # copy calving mask files for exp11-14
         useCalvingMask = False
         if exp_fcg[-2:].isdigit():
             exp_num = int(exp_fcg[-2:])
             if exp_num >= 11:
-                mask_search_path = os.path.join(forcing_basepath, exp_fcg, 'Antarctica_8to30km_ice_shelf_collapse_mask_*.nc')
+                mask_search_path = os.path.join(
+                    forcing_basepath, exp_fcg,
+                    'Antarctica_8to30km_ice_shelf_collapse_mask_*.nc')
                 fcgFileList = glob.glob(mask_search_path)
                 if len(fcgFileList) == 1:
                     mask_path = fcgFileList[0]
@@ -102,35 +121,40 @@ class SetUpExperiment(Step):
                     shutil.copy(mask_path, self.work_dir)
                     useCalvingMask = True
                 else:
-                    sys.exit(f"ERROR: Did not find exactly one matching calving mask file at {mask_search_path}: {fcgFileList}")
+                    sys.exit("ERROR: Did not find exactly one matching "
+                             "calving mask file at "
+                             f"{mask_search_path}: {fcgFileList}")
 
         # Make stream modifications based on files that were determined above
-        stream_replacements = {
-                               'input_file_SMB_forcing': smb_fname,
-                               'input_file_TF_forcing': tf_fname
-                               }
+        stream_replacements = {'input_file_SMB_forcing': smb_fname,
+                               'input_file_TF_forcing': tf_fname}
         if self.exp == 'hist':
             stream_replacements['input_file_init_cond'] = init_cond_fname
             stream_replacements['input_file_region_mask'] = region_mask_fname
             stream_replacements['input_file_melt_params'] = melt_params_fname
         else:
-            stream_replacements['input_file_init_cond'] = 'USE_RESTART_FILE_INSTEAD'
-            stream_replacements['input_file_region_mask'] = 'USE_RESTART_FILE_INSTEAD'
-            stream_replacements['input_file_melt_params'] = 'USE_RESTART_FILE_INSTEAD'
+            stream_replacements['input_file_init_cond'] = \
+                'USE_RESTART_FILE_INSTEAD'
+            stream_replacements['input_file_region_mask'] = \
+                'USE_RESTART_FILE_INSTEAD'
+            stream_replacements['input_file_melt_params'] = \
+                'USE_RESTART_FILE_INSTEAD'
         if self.exp in ['hist', 'ctrlAE']:
             stream_replacements['forcing_interval'] = 'initial_only'
         else:
             stream_replacements['forcing_interval'] = '0001-00-00_00:00:00'
 
         self.add_streams_file(
-            'compass.landice.tests.ismip6_run_ais', 'streams.landice.template',
+            resource_location,
+            'streams.landice.template',
             out_name='streams.landice',
             template_replacements=stream_replacements)
 
         if useCalvingMask:
-            mask_stream_replacements = {'input_file_calving_mask_forcing_name': mask_fname}
+            mask_stream_replacements = {'input_file_calving_mask_forcing_name':
+                                        mask_fname}
             self.add_streams_file(
-                'compass.landice.tests.ismip6_run_ais', 'streams.mask_calving',
+                resource_location, 'streams.mask_calving',
                 out_name='streams.landice',
                 template_replacements=mask_stream_replacements)
 
@@ -138,13 +162,13 @@ class SetUpExperiment(Step):
             vM_param_path = section.get('von_mises_parameter_path')
             vM_stream_replacements = {'input_file_VM_params': vM_param_path}
             self.add_streams_file(
-                'compass.landice.tests.ismip6_run_ais', 'streams.vM_params',
+                resource_location, 'streams.vM_params',
                 out_name='streams.landice',
                 template_replacements=vM_stream_replacements)
 
         # Set up namelist and customize as needed
         self.add_namelist_file(
-            'compass.landice.tests.ismip6_run_ais', 'namelist.landice',
+            resource_location, 'namelist.landice',
             out_name='namelist.landice')
 
         if mesh_res == 4:
@@ -163,8 +187,10 @@ class SetUpExperiment(Step):
         if use_vM_calving:
             options = {'config_calving': "'von_Mises_stress'",
                        'config_restore_calving_front': ".false.",
-                       'config_floating_von_Mises_threshold_stress_source': "'data'",
-                       'config_grounded_von_Mises_threshold_stress_source': "'data'"}
+                       'config_floating_von_Mises_threshold_stress_source':
+                       "'data'",
+                       'config_grounded_von_Mises_threshold_stress_source':
+                       "'data'"}
             self.add_namelist_options(options=options,
                                       out_name='namelist.landice')
 
@@ -174,31 +200,35 @@ class SetUpExperiment(Step):
             self.add_namelist_options(options=options,
                                       out_name='namelist.landice')
 
-        # For all projection runs, symlink the restart file for the historical run
+        # For all projection runs, symlink the restart file for the
+        # historical run
         # don't symlink restart_timestamp or you'll have a mighty mess
         if not self.exp == 'hist':
-            os.symlink(f"../hist/rst.2015-01-01.nc", os.path.join(self.work_dir, 'rst.2015-01-01.nc'))
-            with open(os.path.join(self.work_dir, "restart_timestamp"), "w") as text_file:
+            os.symlink("../hist/rst.2015-01-01.nc",
+                       os.path.join(self.work_dir, 'rst.2015-01-01.nc'))
+            with open(os.path.join(self.work_dir, "restart_timestamp"),
+                      "w") as text_file:
                 text_file.write("2015-01-01_00:00:00")
 
-
         # add the albany_input.yaml file
-        self.add_input_file(filename='albany_input.yaml',
-                            package='compass.landice.tests.ismip6_run_ais',
-                            copy=True)
+        self.add_input_file(
+            filename='albany_input.yaml',
+            package=resource_location,
+            copy=True)
 
         # copy graph files
-        # may be possible to use compass functionality, but wasn't working in this way
-        #make_graph_file(mesh_filename=init_cond_path,
-        #                graph_filename='graph.info')
-        graphFileList = glob.glob(os.path.join(graph_files_path, 'graph.info*'))
+        # may be possible to use compass functionality,
+        # but wasn't working in this way
+        # make_graph_file(mesh_filename=init_cond_path,
+        #                 graph_filename='graph.info')
+        graphFileList = glob.glob(os.path.join(graph_files_path,
+                                               'graph.info*'))
         for gf in graphFileList:
             shutil.copy(gf, self.work_dir)
 
         # provide an example submit script
         template = Template(resources.read_text(
-            'compass.landice.tests.ismip6_run_ais',
-            f'slurm.{mesh_res:02d}.run'))
+            resource_location, f'slurm.{mesh_res:02d}.run'))
         slurm_replacements = {'EXP': f'{self.exp}_{mesh_res:02d}'}
         rendered_text = template.render(slurm_replacements)
         with open(os.path.join(self.work_dir, 'slurm.run'), "w") as fh:
@@ -211,7 +241,7 @@ class SetUpExperiment(Step):
         """
         Run this step of the test case
         """
-        #make_graph_file(mesh_filename=self.mesh_file,
-        #                graph_filename='graph.info')
-        #run_model(step=self, namelist='namelist.landice',
-        #          streams='streams.landice')
+        # make_graph_file(mesh_filename=self.mesh_file,
+        #                 graph_filename='graph.info')
+        # run_model(step=self, namelist='namelist.landice',
+        #           streams='streams.landice')
