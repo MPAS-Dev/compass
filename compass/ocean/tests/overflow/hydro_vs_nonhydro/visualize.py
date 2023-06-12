0cc8e8b51a900856136bb4a9e41df3f01ef987a3
Sara Calandrini <scalandrini@lanl.gov>
2023-06-11 18:15:34 -0600

    modified visualize.py

--- compass/ocean/tests/overflow/hydro_vs_nonhydro/visualize.py
+++ compass/ocean/tests/overflow/hydro_vs_nonhydro/visualize.py
@@ -1,5 +1,6 @@
 import matplotlib
 import matplotlib.pyplot as plt
+import numpy as np
 from netCDF4 import Dataset
 
 from compass.step import Step
@@ -24,43 +25,45 @@ class Visualize(Step):
         """
         super().__init__(test_case=test_case, name='visualize')
 
-        for grid in ['nonhydro', 'hydro']:
-            self.add_input_file(filename=f'output_{grid}.nc',
-                                target=f'../{grid}/output.nc')
-            self.add_input_file(filename=f'init_{grid}.nc',
-                                target=f'../{grid}/init.nc')
+        for mode in ['nonhydro', 'hydro']:
+            self.add_input_file(filename=f'output_{mode}.nc',
+                                target=f'../{mode}/output.nc')
+            self.add_input_file(filename=f'init_{mode}.nc',
+                                target=f'../{mode}/init.nc')
         self.add_output_file('section_overflow.png')
 
     def run(self):
         """
         Run this step of the test case
         """
-        grids = ['hydro', 'nonhydro']
-        nGrids = len(grids)
+        modes = ['hydro', 'nonhydro']
+        nModes = len(modes)
         plt.figure(1, figsize=(12.0, 6.0))
 
         config = self.config
 
         section = config['horizontal_grid']
         nx = section.getint('nx')
+        dc = section.getifloat('dc')
         section = config['visualize']
         time = section.getint('plot_time')
 
-        for j in range(nGrids):
-            grid = grids[j]
-            ncfileIC = Dataset(f'init_{grid}.nc', 'r')
-            ncfile = Dataset(f'output_{grid}.nc', 'r')
+        for j in range(nModes):
+            mode = modes[j]
+            ncfileIC = Dataset(f'init_{mode}.nc', 'r')
+            ncfile = Dataset(f'output_{mode}.nc', 'r')
             temp = ncfile.variables['temperature']
             plt.subplot(2, 1, j + 1)
             plt.imshow(temp[time, 0:nx, :].T)
             plt.clim([10, 20])
             plt.jet()
             plt.colorbar()
-            plt.xticks([0, 50, 100, 150, 200, 250, 300], [0, 1, 2, 3, 4, 5, 6])
+            plt.xticks(np.arange(0., nx, 1000. / dc),
+                       np.arange(0, nx * dc / 1000., 1))
             plt.yticks([0, 30, 60], [0, -100, -200])
             plt.xlabel('x, km')
             plt.ylabel('z, m')
-            plt.title(f'temperature at 3hr - {grid}')
+            plt.title(f'temperature at 3hr - {mode}')
             ncfileIC.close()
             ncfile.close()
 
