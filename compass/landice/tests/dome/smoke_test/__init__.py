504b42cbca7c9ff1dc929a74063101466f154cd4
Trevor Hillebrand <trhille@lanl.gov>
2023-06-27 13:15:58 -0700

    Add new fct advection tests for dome group
    Add new fct advection tests for dome group

--- compass/landice/tests/dome/smoke_test/__init__.py
+++ compass/landice/tests/dome/smoke_test/__init__.py
@@ -1,7 +1,7 @@
-from compass.testcase import TestCase
-from compass.landice.tests.dome.setup_mesh import SetupMesh
 from compass.landice.tests.dome.run_model import RunModel
+from compass.landice.tests.dome.setup_mesh import SetupMesh
 from compass.landice.tests.dome.visualize import Visualize
+from compass.testcase import TestCase
 
 
 class SmokeTest(TestCase):
@@ -16,9 +16,12 @@ class SmokeTest(TestCase):
 
     velo_solver : {'sia', 'FO'}
         The velocity solver to use for the test case
+
+    advection_type : {'fo', 'fct'}
+        The type of advection to use for thickness and tracers
     """
 
-    def __init__(self, test_group, velo_solver, mesh_type):
+    def __init__(self, test_group, velo_solver, mesh_type, advection_type):
         """
         Create the test case
 
@@ -32,11 +35,16 @@ class SmokeTest(TestCase):
 
         mesh_type : str
             The resolution or type of mesh of the test case
+
+        advection_type : {'fo', 'fct'}
+            The type of advection to use for thickness and tracers
         """
         name = 'smoke_test'
         self.mesh_type = mesh_type
         self.velo_solver = velo_solver
-        subdir = '{}/{}_{}'.format(mesh_type, velo_solver.lower(), name)
+        self.advection_type = advection_type
+        subdir = '{}/{}_{}_{}'.format(mesh_type, velo_solver.lower(),
+                                      advection_type, name)
         super().__init__(test_group=test_group, name=name,
                          subdir=subdir)
 
@@ -49,6 +57,10 @@ class SmokeTest(TestCase):
         if velo_solver == 'sia':
             step.add_namelist_options(
                 {'config_run_duration': "'0200-00-00_00:00:00'"})
+        if advection_type == 'fct':
+            step.add_namelist_options(
+                {'config_thickness_advection': "'fct'",
+                 'config_tracer_advection': "'fct'"})
         self.add_step(step)
 
         step = Visualize(test_case=self, mesh_type=mesh_type)
