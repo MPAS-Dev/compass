42f1207dd45ad10f0eb0cace6a28110d4d0f8273
Matthew Hoffman <mhoffman@lanl.gov>
2023-08-26 09:49:25 -0700

    Add thickness advection version of horizontal_advection test
    This is identical to the horizontal_advection test of a passive tracer, but Gaussian
    bump is applied to thickness field and tracer advection is not considered.  This
    isolates the analysis to a single advection call; if there are issues with the
    thickness advection, we presumably cannot expect tracer advection to converge properly.
    
    It might have been possible to create an argument to toggle between
    versions, but it was easier (and arguably less confusing) to just make a
    copy.
    
    This uses a Gaussian bump of 1000 m sitting on top of a 1000 m base
    layer in a doubly periodic mesh.  This ensures a margin-less domain,
    which would avoid the issue of advection degrading to 1st order at
    margins.

--- compass/landice/tests/mesh_convergence/__init__.py
+++ compass/landice/tests/mesh_convergence/__init__.py
@@ -2,6 +2,9 @@ from compass.landice.tests.mesh_convergence.halfar import Halfar
 from compass.landice.tests.mesh_convergence.horizontal_advection import (
     HorizontalAdvection,
 )
+from compass.landice.tests.mesh_convergence.horizontal_advection_thickness import (  # noqa
+    HorizontalAdvectionThickness,
+)
 from compass.testgroup import TestGroup
 
 
@@ -17,4 +20,5 @@ class MeshConvergence(TestGroup):
         super().__init__(mpas_core=mpas_core, name='mesh_convergence')
 
         self.add_test_case(HorizontalAdvection(test_group=self))
+        self.add_test_case(HorizontalAdvectionThickness(test_group=self))
         self.add_test_case(Halfar(test_group=self))
