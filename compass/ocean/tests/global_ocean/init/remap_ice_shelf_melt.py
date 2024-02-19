06bc5a86c84b42b1c78c49b1bee5f79da2ea2ea8
Xylar Asay-Davis <xylarstorm@gmail.com>
2024-02-19 09:37:13 -0600

    Reroute missing fluxes in data ice-shelf melt

--- compass/ocean/tests/global_ocean/init/remap_ice_shelf_melt.py
+++ compass/ocean/tests/global_ocean/init/remap_ice_shelf_melt.py
@@ -5,7 +5,9 @@ import xarray as xr
 from mpas_tools.cime.constants import constants
 from mpas_tools.io import write_netcdf
 from pyremap import MpasCellMeshDescriptor, ProjectionGridDescriptor, Remapper
+from scipy.spatial import KDTree
 
+from compass.ocean.mesh.cull import write_map_culled_to_base
 from compass.step import Step
 
 
@@ -33,15 +35,24 @@ class RemapIceShelfMelt(Step):
         super().__init__(test_case, name='remap_ice_shelf_melt', ntasks=512,
                          min_tasks=1)
 
-        mesh_path = mesh.get_cull_mesh_path()
+        base_mesh_path = mesh.steps['base_mesh'].path
+        culled_mesh_path = mesh.steps['cull_mesh'].path
 
         self.add_input_file(
-            filename='mesh.nc',
-            work_dir_target=f'{mesh_path}/culled_mesh.nc')
+            filename='base_mesh.nc',
+            work_dir_target=f'{base_mesh_path}/base_mesh.nc')
+
+        self.add_input_file(
+            filename='culled_mesh.nc',
+            work_dir_target=f'{culled_mesh_path}/culled_mesh.nc')
+
+        self.add_input_file(
+            filename='map_culled_to_base.nc',
+            work_dir_target=f'{culled_mesh_path}/map_culled_to_base.nc')
 
         self.add_input_file(
             filename='land_ice_mask.nc',
-            work_dir_target=f'{mesh_path}/land_ice_mask.nc')
+            work_dir_target=f'{culled_mesh_path}/land_ice_mask.nc')
 
         self.add_input_file(
             filename='Adusumilli_2020_iceshelf_melt_rates_2010-2018_v0.h5',
@@ -67,21 +78,26 @@ class RemapIceShelfMelt(Step):
 
         parallel_executable = config.get('parallel', 'parallel_executable')
 
-        mesh_filename = 'mesh.nc'
+        base_mesh_filename = 'base_mesh.nc'
+        culled_mesh_filename = 'culled_mesh.nc'
         land_ice_mask_filename = 'land_ice_mask.nc'
+        map_culled_to_base_filename = 'map_culled_to_base.nc'
         mesh_name = self.mesh.mesh_name
 
-        remap_adusumilli(in_filename, mesh_filename, mesh_name,
-                         land_ice_mask_filename, out_filename,
-                         logger=logger, mpi_tasks=ntasks,
-                         parallel_executable=parallel_executable)
+        remap_adusumilli(
+            in_filename, base_mesh_filename, culled_mesh_filename,
+            mesh_name, land_ice_mask_filename, out_filename,
+            logger=logger, mpi_tasks=ntasks,
+            parallel_executable=parallel_executable,
+            map_culled_to_base_filename=map_culled_to_base_filename)
 
 
-def remap_adusumilli(in_filename, mesh_filename, mesh_name,
-                     land_ice_mask_filename, out_filename, logger,
+def remap_adusumilli(in_filename, base_mesh_filename, culled_mesh_filename,
+                     mesh_name, land_ice_mask_filename, out_filename, logger,
                      mapping_directory='.', method='conserve',
                      renormalization_threshold=None, mpi_tasks=1,
-                     parallel_executable=None):
+                     parallel_executable=None,
+                     map_culled_to_base_filename=None):
     """
     Remap the Adusumilli et al. (2020) melt rates at 1 km resolution to an MPAS
     mesh
@@ -91,8 +107,11 @@ def remap_adusumilli(in_filename, mesh_filename, mesh_name,
     in_filename : str
         The original Adusumilli et al. (2020) melt rates
 
-    mesh_filename : str
-        The MPAS mesh
+    base_mesh_filename : str
+        The base MPAS mesh before land is culled
+
+    culled_mesh_filename : str
+        The MPAS mesh after land has been culled
 
     mesh_name : str
         The name of the mesh (e.g. oEC60to30wISC), used in the name of the
@@ -127,6 +146,10 @@ def remap_adusumilli(in_filename, mesh_filename, mesh_name,
     parallel_executable : {'srun', 'mpirun'}, optional
         The name of the parallel executable to use to launch ESMF tools.
         But default, 'mpirun' from the conda environment is used
+
+    map_culled_to_base_filename : str, optional
+        A file with indices that map from the culled to the base MPAS mesh. If
+        not provided, they will be computed
     """
 
     logger.info(f'Reading {in_filename}...')
@@ -172,10 +195,10 @@ def remap_adusumilli(in_filename, mesh_filename, mesh_name,
     write_netcdf(ds, 'Adusumilli_2020_ismf_2010-2018_v0.nc')
     logger.info('done.')
 
-    out_descriptor = MpasCellMeshDescriptor(mesh_filename, mesh_name)
+    out_descriptor = MpasCellMeshDescriptor(base_mesh_filename, mesh_name)
 
     mapping_filename = \
-        f'{mapping_directory}/map_{in_grid_name}_to_{mesh_name}.nc'
+        f'{mapping_directory}/map_{in_grid_name}_to_{mesh_name}_base.nc'
 
     logger.info(f'Creating the mapping file {mapping_filename}...')
     remapper = Remapper(in_descriptor, out_descriptor, mapping_filename)
@@ -190,22 +213,126 @@ def remap_adusumilli(in_filename, mesh_filename, mesh_name,
         ds, renormalizationThreshold=renormalization_threshold)
     logger.info('done.')
 
-    ds_mask = xr.open_dataset(land_ice_mask_filename)
-    mask = ds_mask.landIceMask
+    if map_culled_to_base_filename is None:
+        map_culled_to_base_filename = 'map_culled_to_base.nc'
+        write_map_culled_to_base(base_mesh_filename=base_mesh_filename,
+                                 culled_mesh_filename=culled_mesh_filename,
+                                 out_filename=map_culled_to_base_filename)
 
-    for field in ['dataLandIceFreshwaterFlux',
-                  'dataLandIceHeatFlux']:
-        # zero out the field where it's currently NaN, and mask only to
-        # regions with land ice
-        ds_remap[field] = \
-            mask * ds_remap[field].where(ds_remap[field].notnull(), 0.)
+    _land_ice_mask_on_base_mesh(
+        base_mesh_filename=base_mesh_filename,
+        land_ice_mask_filename=land_ice_mask_filename,
+        map_culled_to_base_filename=map_culled_to_base_filename)
 
-        # add a time dimension
-        if 'Time' not in ds_remap[field].dims:
-            ds_remap[field] = ds_remap[field].expand_dims(dim='Time', axis=0)
-
-    # deal with melting beyond the land-ice mask
-
+    ds_mask = xr.open_dataset('land_ice_mask_on_base.nc')
+    mask = ds_mask.landIceFloatingMask
+    ds_remap['landIceFloatingMask'] = mask
     ds_remap.attrs.pop('history')
 
-    write_netcdf(ds_remap, out_filename)
+    write_netcdf(ds_remap, 'ismf_remapped_to_base.nc')
+
+    # deal with melting beyond the land-ice mask
+    _reroute_missing_flux(base_mesh_filename, map_culled_to_base_filename,
+                          out_filename, logger)
+
+
+def _land_ice_mask_on_base_mesh(base_mesh_filename, land_ice_mask_filename,
+                                map_culled_to_base_filename):
+    """ Map the land-ice mask back to the base mesh """
+
+    ds_map = xr.open_dataset(map_culled_to_base_filename)
+    map_culled_to_base = ds_map.mapCulledToBase.values
+
+    ds_base = xr.open_dataset(base_mesh_filename)
+    ncells_base = ds_base.sizes['nCells']
+
+    ds_culled_mask = xr.open_dataset(land_ice_mask_filename)
+    culled_land_ice_mask = ds_culled_mask.landIceFloatingMask.values
+    base_land_ice_mask = np.zeros(ncells_base, dtype=int)
+    base_land_ice_mask[map_culled_to_base] = culled_land_ice_mask
+    ds_base_mask = xr.Dataset()
+    ds_base_mask['landIceFloatingMask'] = ('nCells', base_land_ice_mask)
+
+    write_netcdf(ds_base_mask, 'land_ice_mask_on_base.nc')
+
+
+def _reroute_missing_flux(base_mesh_filename, map_culled_to_base_filename,
+                          out_filename, logger):
+    """
+    For each flux cell not within an ice shelf, find the closest ice-shelf cell
+    """
+    ds_ismf_base = xr.open_dataset('ismf_remapped_to_base.nc')
+    land_ice_mask = ds_ismf_base.landIceFloatingMask
+    fwf = ds_ismf_base.dataLandIceFreshwaterFlux
+    fwf = fwf.where(fwf.notnull(), 0.)
+    hf = ds_ismf_base.dataLandIceHeatFlux
+    hf = hf.where(hf.notnull(), 0.)
+    flux_mask = fwf != 0.
+    fluxes_to_reroute = np.logical_and(flux_mask,
+                                       land_ice_mask != 1)
+
+    ds_base = xr.open_dataset(base_mesh_filename)
+    ncells_base = ds_base.sizes['nCells']
+    base_xyz = np.zeros((ncells_base, 3))
+    base_xyz[:, 0] = ds_base.xCell.values
+    base_xyz[:, 1] = ds_base.yCell.values
+    base_xyz[:, 2] = ds_base.zCell.values
+    area = ds_base.areaCell
+
+    land_ice_xyz = base_xyz[land_ice_mask.values == 1, :]
+    land_ice_cell_indices = np.arange(ncells_base)[land_ice_mask.values == 1]
+
+    reroute_mask = fluxes_to_reroute.astype(int)
+
+    fw_mass_rerouted = (fwf * area).isel(nCells=fluxes_to_reroute.values)
+    heat_rerouted = (hf * area).isel(nCells=fluxes_to_reroute.values)
+
+    count = np.sum(reroute_mask.values)
+    logger.info(f'Rerouting fluxes from {count} cells')
+    fwf_land_ice = (land_ice_mask * fwf * area).sum().values
+    logger.info(f'Captured flux (kg/s):         {fwf_land_ice:.1f}')
+    fwf_rerouted = fw_mass_rerouted.sum().values
+    logger.info(f'Rerouted flux (kg/s):         {fwf_rerouted:.1f}')
+    fwf_total = (fwf * area).sum().values
+    logger.info(f'Total flux (kg/s):            {fwf_total:.1f}')
+    ds_to_route = xr.Dataset()
+    ds_to_route['rerouteMask'] = reroute_mask
+    write_netcdf(ds_to_route, 'route_mask.nc')
+
+    reroute_xyz = base_xyz[fluxes_to_reroute.values, :]
+
+    # we want to find closest land-ice cell to a given reroute cell
+    tree = KDTree(land_ice_xyz)
+
+    # todo: may need to be more sophisticated with workers based on ntasks,
+    # etc.
+    _, indices = tree.query(reroute_xyz, workers=-1)
+
+    ds_map = xr.open_dataset(map_culled_to_base_filename)
+    map_culled_to_base = ds_map.mapCulledToBase.values
+
+    fwf_rerouted = land_ice_mask.values * fwf.where(fwf.notnull(), 0.).values
+    hf_rerouted = land_ice_mask.values * hf.where(hf.notnull(), 0.).values
+    for rerouted_index, land_ice_index in enumerate(indices):
+        icell = land_ice_cell_indices[land_ice_index]
+        fwf_rerouted[icell] += fw_mass_rerouted[rerouted_index] / area[icell]
+        hf_rerouted[icell] += heat_rerouted[rerouted_index] / area[icell]
+
+    # now, go from base to culled mesh
+    fwf_rerouted = fwf_rerouted[map_culled_to_base]
+    hf_rerouted = hf_rerouted[map_culled_to_base]
+
+    ds_out = xr.Dataset()
+    field = 'dataLandIceFreshwaterFlux'
+    ds_out[field] = ('nCells', fwf_rerouted)
+    ds_out[field] = ds_out[field].expand_dims(dim='Time', axis=0)
+    ds_out[field].attrs['units'] = 'kg m^-2 s^-1'
+    field = 'dataLandIceHeatFlux'
+    ds_out[field] = ('nCells', hf_rerouted)
+    ds_out[field] = ds_out[field].expand_dims(dim='Time', axis=0)
+    ds_out[field].attrs['units'] = 'W m^-2'
+    write_netcdf(ds_out, out_filename)
+
+    fwf_total = (ds_out.dataLandIceFreshwaterFlux *
+                 area.isel(nCells=map_culled_to_base)).sum().values
+    logger.info(f'Total after rerouting (kg/s): {fwf_total:.1f}')
