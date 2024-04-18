6f6c14e2b0e0dbedad378955791900a94e2b9d15
Erin Thomas <ac.ethomas@chrlogin2.lcrc.anl.gov>
2024-04-17 21:58:47 -0500

    scrip file creation

---
+++ compass/ocean/tests/global_ocean/wave_mesh/scrip_file.py
@@ -0,0 +1,37 @@
+from mpas_tools.logging import check_call
+
+from compass import Step
+
+
+class WavesScripFile(Step):
+    """
+    A step for creating the scrip file for the wave mesh
+    """
+    def __init__(self, test_case, wave_culled_mesh,
+                 name='scrip_file', subdir=None):
+
+        super().__init__(test_case=test_case, name=name, subdir=subdir)
+
+        wave_culled_mesh_path = wave_culled_mesh.path
+        self.add_input_file(
+            filename='wave_mesh_culled.msh',
+            work_dir_target=f'{wave_culled_mesh_path}/wave_mesh_culled.msh')
+
+    def setup(self):
+
+        super().setup()
+
+        f = open(f'{self.work_dir}/scrip.nml', 'w')
+        f.write('&inputs\n')
+        f.write("   waves_mesh_file = 'waves_mesh_culled.msh'\n")
+        f.write("/")
+        f.write('&outputs\n')
+        f.write("   waves_scrip_file = 'waves_mesh_scrip.nc'\n")
+        f.write("/")
+        f.close()
+
+    def run(self):
+        """
+        Create scrip files for wave mesh
+        """
+        check_call('ocean_scrip_wave_mesh', logger=self.logger)
