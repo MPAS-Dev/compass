7e37003c5938ec193d234bf8cc51cb1c248db72e
Alice Barthel <abarthel@lanl.gov>
2024-08-01 13:48:09 -0500

    added spinup timeseries plot

--- compass/ocean/tests/baroclinic_gyre/viz.py
+++ compass/ocean/tests/baroclinic_gyre/viz.py
@@ -36,11 +36,13 @@ class Viz(Step):
 
         # config = self.config
         out_dir = '.'
-        sim_dir = '../moc'
+        moc_dir = '../moc'
+        mon_dir = '../forward/output'
         dsMesh = xarray.open_dataset('../initial_state/initial_state.nc')
-        ds = xarray.open_dataset(f'{sim_dir}/moc.nc')
+        ds = xarray.open_dataset(f'{moc_dir}/moc.nc')
         # Insert plots here
         self._plot_moc(ds, dsMesh, out_dir)
+        self._plot_spinup(mon_dir, dsMesh, out_dir)
 
     def _plot_moc(self, ds, dsMesh, out_dir):
         """
@@ -65,3 +67,31 @@ class Viz(Step):
                      xy=(0.01, 0.05), xycoords='axes fraction')
         plt.colorbar()
         plt.savefig('{}/time_avg_moc.png'.format(out_dir))
+
+    def _plot_spinup(self, mon_dir, dsMesh, out_dir):
+        """
+        Plot the timeseries of monthy layer-mean
+        kinetic energy and temperature for the test case
+        """
+
+        ds = xarray.open_mfdataset(
+            '{}/timeSeriesStatsMonthly*.nc'.format(mon_dir),
+            concat_dim='Time', combine='nested')
+        KE = ds.timeMonthly_avg_kineticEnergyCell[:, :, :].mean(axis=1)
+        T = ds.timeMonthly_avg_activeTracers_temperature[:, :, :].mean(axis=1)
+        midlayer = (dsMesh.refInterfaces +
+                    0.5 *
+                    (np.roll(dsMesh.refInterfaces, -1) - dsMesh.refInterfaces)
+                    ).values[:-1]
+
+        fig, ax = plt.subplots(2, 1, figsize=(6, 8))
+        for ll in [0, 3, 6, 10, 14]:
+            ax[0].plot(KE[:, ll], label='{}m'.format(int(midlayer[ll])))
+            ax[1].plot(T[:, ll], label='{}m'.format(int(midlayer[ll])))
+        ax[0].legend()
+        ax[1].legend()
+        ax[0].set_xlabel('Time (months)')
+        ax[1].set_xlabel('Time (months)')
+        ax[0].set_ylabel('Layer Mean Kinetic Energy ($m^2 s^{-2}$)')
+        ax[1].set_ylabel(r'Layer Mean Temperature ($^{\circ}$C)')
+        plt.savefig('{}/spinup_ft.png'.format(out_dir), bbox_inches='tight')
