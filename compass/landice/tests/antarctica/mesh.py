9db1c46ecfeb8853f6d58dbc36a3e1c30358b8de
Cameron Smith <smithc11@rpi.edu>
2025-02-21 09:40:44 -0500

    update AIS meshgen call to interp_gridded2mali

--- compass/landice/tests/antarctica/mesh.py
+++ compass/landice/tests/antarctica/mesh.py
@@ -138,14 +138,16 @@ class Mesh(Step):
 
         # Now perform bespoke interpolation of geometry and velocity data
         # from their respective sources
-        interp_gridded2mali(self, bedmachine_dataset, dst_scrip_file, nProcs,
+        interp_gridded2mali(self, bedmachine_dataset, dst_scrip_file, 
+                            parallel_executable, nProcs,
                             self.mesh_filename, src_proj, variables="all")
 
         # only interpolate a subset of MEaSUREs variables onto the MALI mesh
         measures_vars = ['observedSurfaceVelocityX',
                          'observedSurfaceVelocityY',
                          'observedSurfaceVelocityUncertainty']
-        interp_gridded2mali(self, measures_dataset, dst_scrip_file, nProcs,
+        interp_gridded2mali(self, measures_dataset, dst_scrip_file,
+                            parallel_executable, nProcs,
                             self.mesh_filename, src_proj,
                             variables=measures_vars)
 
