27eae0f3b5e524bc3889c1987c3e980d65205032
Steven Brus <steven.r.brus@gmail.com>
2025-08-27 20:50:34 -0700

    Fix bug in initial state for final subgrid file

--- compass/ocean/tests/hurricane/init/initial_state.py
+++ compass/ocean/tests/hurricane/init/initial_state.py
@@ -233,7 +233,7 @@ class InitialState(Step):
 
         mesh = nc.Dataset("mesh.nc", "r")
         if self.wetdry == 'subgrid':
-            init = nc.Dataset(f"ocean_subgrid{i + 1}.nc", "r+")
+            init = nc.Dataset("ocean_subgrid_final.nc", "r+")
         else:
             init = nc.Dataset("ocean.nc", "r+")
 
