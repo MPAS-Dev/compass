b7bf47edcb21b109ac3b9c42e1b49db67a9d4aac
Steven Brus <steven.r.brus@gmail.com>
2025-10-21 12:29:36 -0700

    Clean up buttermilk bay test case

--- compass/ocean/tests/buttermilk_bay/initial_state.py
+++ compass/ocean/tests/buttermilk_bay/initial_state.py
@@ -40,7 +40,7 @@ class InitialState(Step):
         self.add_input_file(
             filename='buttermilk_bathy.nc',
             target='buttermilk_bathy.nc',
-            database='bathymetry_database')
+            database='buttermilk_bay')
 
         for file in ['base_mesh.nc', 'culled_mesh.nc', 'culled_graph.info',
                      'ocean.nc', 'init_mode_forcing_data.nc']:
