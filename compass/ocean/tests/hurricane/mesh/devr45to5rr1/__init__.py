5b813352eafb819c8ed50a6deef9bf9d2069fc19
Steven Brus <steven.r.brus@gmail.com>
2025-10-21 14:28:57 -0700

    Remove highest resolution Delware Bay refinement
     - This was down to .6 km

--- compass/ocean/tests/hurricane/mesh/devr45to5rr1/__init__.py
+++ compass/ocean/tests/hurricane/mesh/devr45to5rr1/__init__.py
@@ -7,12 +7,12 @@ from compass.ocean.tests.tides.mesh.vr45to5 import VRTidesMesh
 
 class DEVR45to5rr1BaseMesh(VRTidesMesh, FloodplainMeshStep):
     """
-    A step for creating DEQU120at30cr10rr2 meshes
+    A step for creating DEVR45to5rr1BaseMesh meshes
     """
     def region_multiplier(self, xgrid):
         """
-        Create cell width array for this mesh on a regular latitude-longitude
-        grid
+        Create cell width multiplier array for this mesh on a
+        regular latitude-longitude grid
 
         Returns
         -------
@@ -30,7 +30,7 @@ class DEVR45to5rr1BaseMesh(VRTidesMesh, FloodplainMeshStep):
         params = ct.default_params
         params['ddeg'] = xgrid[1] - xgrid[0]
 
-        # QU 120 background mesh and enhanced Atlantic (30km)
+        # Background
         params["mesh_type"] = "QU"
         params["dx_max_global"] = 1.0
         params["region_box"] = ct.Atlantic
@@ -40,49 +40,29 @@ class DEVR45to5rr1BaseMesh(VRTidesMesh, FloodplainMeshStep):
         params["trans_width"] = 5000.0 * km
         params["trans_start"] = 500.0 * km
 
-        cell_width, lon, lat = ct.coastal_refined_mesh(params)
+        multiplier, lon, lat = ct.coastal_refined_mesh(params)
 
-        # Northeast refinement (10km)
+        # Northeast refinement
         params["region_box"] = ct.Delaware_Bay
         params["plot_box"] = ct.Western_Atlantic
         params["dx_min_coastal"] = 0.5
         params["trans_width"] = 600.0 * km
         params["trans_start"] = 400.0 * km
 
-        cell_width, lon, lat = ct.coastal_refined_mesh(
-            params, cell_width, lon, lat)
+        multiplier, lon, lat = ct.coastal_refined_mesh(
+            params, multiplier, lon, lat)
 
-        # Delaware regional refinement (6km)
+        # Delaware regional refinement (1.25 km)
         params["region_box"] = ct.Delaware_Region
         params["plot_box"] = ct.Delaware
         params["dx_min_coastal"] = 0.25
         params["trans_width"] = 175.0 * km
         params["trans_start"] = 75.0 * km
 
-        cell_width, lon, lat = ct.coastal_refined_mesh(
-            params, cell_width, lon, lat)
+        multiplier, lon, lat = ct.coastal_refined_mesh(
+            params, multiplier, lon, lat)
 
-        # Delaware Bay high-resolution (2km)
-        Delaware_restrict = {"include": [np.array([[-75.853, 39.732],
-                                                   [-74.939, 36.678],
-                                                   [-71.519, 40.156],
-                                                   [-74.784, 40.296]]),
-                                         np.array([[-76.024, 37.188],
-                                                   [-75.214, 36.756],
-                                                   [-74.512, 37.925],
-                                                   [-75.274, 38.318]])],
-                             "exclude": []}
-        params["region_box"] = ct.Delaware_Bay
-        params["plot_box"] = ct.Delaware
-        params["restrict_box"] = Delaware_restrict
-        params["dx_min_coastal"] = 0.125
-        params["trans_width"] = 100.0 * km
-        params["trans_start"] = 17.0 * km
-
-        cell_width, lon, lat = ct.coastal_refined_mesh(
-            params, cell_width, lon, lat)
-
-        return cell_width, lon, lat
+        return multiplier, lon, lat
 
     def build_cell_width_lat_lon(self):
 
